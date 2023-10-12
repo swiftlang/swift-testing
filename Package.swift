2e6ae7b742b9e296de392f0d6754bd2071e5f9c3
Jonathan Grynspan <jgrynspan@apple.com>
2023-10-12 12:46:32 -0400

    Fix the 256-color terminal check so it doesn't require `_StringProcessing`.
    Use `strstr()` so that we can detect 256-color terminal support on older macOS versions or with older toolchains that don't have the `_StringProcessing` module.
    
    I did _not_ change another use of `contains()` in ExpectationChecking+Macro.swift because it may be sensitive to Unicode nuances whereas a search for the ASCII string ``"256"` isn't.
    
    I also took the opportunity to replace one other usage of `#available(explicit_versions_here)` with an availability define. I sorted the availability defines in the package manifest too.

--- Package.swift
+++ Package.swift
@@ -114,11 +114,13 @@ extension Array where Element == PackageDescription.SwiftSetting {
         "-strict-concurrency=complete",
         "-require-explicit-sendable",
 
+        "-Xfrontend", "-define-availability", "-Xfrontend", "_mangledTypeNameAPI:macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0",
+        "-Xfrontend", "-define-availability", "-Xfrontend", "_backtraceAsyncAPI:macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0",
         "-Xfrontend", "-define-availability", "-Xfrontend", "_clockAPI:macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0",
-        "-Xfrontend", "-define-availability", "-Xfrontend", "_distantFuture:macOS 99.0, iOS 99.0, watchOS 99.0, tvOS 99.0",
         "-Xfrontend", "-define-availability", "-Xfrontend", "_regexAPI:macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0",
         "-Xfrontend", "-define-availability", "-Xfrontend", "_swiftVersionAPI:macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0",
-        "-Xfrontend", "-define-availability", "-Xfrontend", "_mangledTypeNameAPI:macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0",
+
+        "-Xfrontend", "-define-availability", "-Xfrontend", "_distantFuture:macOS 99.0, iOS 99.0, watchOS 99.0, tvOS 99.0",
       ]),
       .enableUpcomingFeature("ExistentialAny"),
       .define("SWT_TARGET_OS_APPLE", .when(platforms: [.macOS, .iOS, .macCatalyst, .watchOS, .tvOS, .visionOS])),
