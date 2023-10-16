83ccb5c30b05894523be0e616ee94bf9920a0b9a
Jonathan Grynspan <jgrynspan@apple.com>
2023-10-16 09:37:46 -0400

    Update Sources/Testing/Events/Event.swift
    Co-authored-by: Stuart Montgomery <smontgomery@apple.com>

--- Sources/Testing/Events/Event.swift
+++ Sources/Testing/Events/Event.swift
@@ -35,7 +35,7 @@ public struct Event: Sendable {
     ///
     /// The test that started is contained in the ``Event/Context`` instance
     /// that was passed to the event handler along with this event. Its ID is
-    /// available from the event's ``Event/testID`` property.
+    /// available from this event's ``Event/testID`` property.
     case testStarted
 
     /// A test case started.
