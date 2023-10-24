005c0a09c094aed5879adbdd11c71cdd15402d83
Dennis Weissmann <dennisweissmann@users.noreply.github.com>
2023-10-23 19:39:16 -0700

    Create a `Expectation.Snapshot` type that conforms to `Codable` (#73)
    Create a `Expectation.Snapshot` type that conforms to `Codable`

--- Sources/Testing/Expectations/Expectation.swift
+++ Sources/Testing/Expectations/Expectation.swift
@@ -62,3 +62,65 @@ public struct ExpectationFailedError: Error {
   /// The expectation that failed.
   public var expectation: Expectation
 }
+
+// MARK: - Snapshot
+
+extension Expectation {
+  /// A serializable type describing an expectation that has been evaluated.
+  @_spi(ExperimentalSnapshotting)
+  public struct Snapshot: Sendable, Codable {
+    /// The source code description (as a String) of the expression evaluated by
+    /// this expectation, if available at compile time.
+    public var sourceCodeDescription: String?
+
+    /// A description of the error mismatch that occurred, if any.
+    ///
+    /// If this expectation passed, the value of this property is `nil` because no
+    /// error mismatch occurred.
+    @_spi(ExperimentalEventHandling)
+    public var mismatchedErrorDescription: String?
+
+    /// A description of the expression evaluated by this expectation, expanded
+    /// to include the values of any evaluated sub-expressions, if the source code
+    /// was available at compile time.
+    ///
+    /// If this expectation passed, the value of this property is `nil` because
+    /// source code expansion is only performed when necessary to assist with
+    /// diagnosing test failures.
+    @_spi(ExperimentalEventHandling)
+    public var expandedExpressionDescription: String?
+
+    /// A description of the difference between the operands in the expression
+    /// evaluated by this expectation, if the difference could be determined.
+    ///
+    /// If this expectation passed, the value of this property is `nil` because
+    /// the difference is only computed when necessary to assist with diagnosing
+    /// test failures.
+    @_spi(ExperimentalEventHandling)
+    public var differenceDescription: String?
+
+    /// Whether the expectation passed or failed.
+    ///
+    /// An expectation is considered to pass when its condition evaluates to
+    /// `true`. If it evaluates to `false`, it fails instead.
+    public var isPassing: Bool
+
+    /// Whether or not the expectation was required to pass.
+    public var isRequired: Bool
+
+    /// The source location where this expectation was evaluated.
+    public var sourceLocation: SourceLocation
+
+    /// Creates a snapshot expectation from a real ``Expectation``.
+    /// - Parameter expectation: The real expectation.
+    init(expectation: Expectation) {
+      self.sourceCodeDescription = String(describing: expectation.sourceCode)
+      self.mismatchedErrorDescription = expectation.mismatchedErrorDescription
+      self.expandedExpressionDescription = expectation.expandedExpressionDescription
+      self.differenceDescription = expectation.differenceDescription
+      self.isPassing = expectation.isPassing
+      self.isRequired = expectation.isRequired
+      self.sourceLocation = expectation.sourceLocation
+    }
+  }
+}
