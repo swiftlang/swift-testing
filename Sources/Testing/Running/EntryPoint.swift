ab0ab987c41d97e913b8c086c2325116c27340c2
Jonathan Grynspan <jgrynspan@apple.com>
2023-10-26 19:19:12 -0400

    Add a "main" entry point function for use by SPM. (#81)
    * Add a "main" entry point function for use by SPM.
    
    This PR adds a new symbol, `swiftPMEntryPoint()`, that can be called by Swift Package Manager to trigger running tests. This function shares most of its implementation with `XCTestScaffold`, so that function's implementation has been factored out into a common function both call.
    
    ---------
    
    Co-authored-by: Stuart Montgomery <smontgomery@apple.com>

---
+++ Sources/Testing/Running/EntryPoint.swift
@@ -0,0 +1,192 @@
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
+
+@_implementationOnly import TestingInternals
+
+/// The entry point to the testing library used by Swift Package Manager.
+///
+/// - Returns: The result of invoking the testing library. The type of this
+///   value is subject to change.
+///
+/// This function examines the command-line arguments to the current process
+/// and then invokes available tests in the current process.
+///
+/// - Warning: This function is used by Swift Package Manager. Do not call it
+///   directly.
+@_spi(SwiftPackageManagerSupport)
+@_disfavoredOverload
+public func swiftPMEntryPoint() async -> CInt {
+  let args = CommandLine.arguments()
+  _ = args
+
+  @Locked var exitCode = EXIT_SUCCESS
+
+  await runTests { event, _ in
+    if case let .issueRecorded(issue) = event.kind, !issue.isKnown {
+      $exitCode.withLock { exitCode in
+        exitCode = EXIT_FAILURE
+      }
+    }
+  }
+
+  return exitCode
+}
+
+/// The entry point to the testing library used by Swift Package Manager.
+///
+/// This function examines the command-line arguments to the current process
+/// and then invokes available tests in the current process. When the tests
+/// complete, the process is terminated. If tests were successful, an exit code
+/// of `EXIT_SUCCESS` is used; otherwise, a (possibly platform-specific) value
+/// such as `EXIT_FAILURE` is used instead.
+///
+/// - Warning: This function is used by Swift Package Manager. Do not call it
+///   directly.
+@_spi(SwiftPackageManagerSupport)
+public func swiftPMEntryPoint() async -> Never {
+  let exitCode: CInt = await swiftPMEntryPoint()
+  exit(exitCode)
+}
+
+/// The common implementation of ``swiftPMEntryPoint()`` and
+/// ``XCTestScaffold/runAllTests(hostedBy:)``.
+///
+/// - Parameters:
+///   - testIDs: The test IDs to run. If `nil`, all tests are run.
+///   - tags: The tags to filter by (only tests with one or more of these tags
+///     will be run.)
+///   - eventHandler: An event handler to invoke after events are written to
+///     the standard error stream.
+func runTests(identifiedBy testIDs: [Test.ID]? = nil, taggedWith tags: Set<Tag>? = nil, eventHandler: @escaping Event.Handler = { _, _ in }) async {
+  let eventRecorder = Event.Recorder(options: .forStandardError) { string in
+    let stderr = swt_stderr()
+    fputs(string, stderr)
+    fflush(stderr)
+  }
+
+  var configuration = Configuration()
+  configuration.isParallelizationEnabled = false
+  configuration.eventHandler = { event, context in
+    eventRecorder.record(event, in: context)
+    eventHandler(event, context)
+  }
+  
+  if let testIDs {
+    configuration.setTestFilter(toMatch: Set(testIDs))
+  }
+  if let tags {
+    // Check if the test's tags intersect the set of selected tags. If there
+    // was a previous filter function, it must also pass.
+    let oldTestFilter = configuration.testFilter ?? { _ in true }
+    configuration.testFilter = { test in
+      !tags.isDisjoint(with: test.tags) && oldTestFilter(test)
+    }
+  }
+
+  let runner = await Runner(configuration: configuration)
+  await runner.run()
+}
+
+// MARK: - Command-line interface options
+
+extension [Event.Recorder.Option] {
+  /// The set of options to use when writing to the standard error stream.
+  static var forStandardError: Self {
+    var result = Self()
+
+    let useANSIEscapeCodes = _standardErrorSupportsANSIEscapeCodes
+    if useANSIEscapeCodes {
+      result.append(.useANSIEscapeCodes)
+      if _standardErrorSupports256ColorANSIEscapeCodes {
+        result.append(.use256ColorANSIEscapeCodes)
+      }
+    }
+
+#if os(macOS) || (os(iOS) && targetEnvironment(macCatalyst))
+    // On macOS, if we are writing to a TTY (i.e. Terminal.app) and the SF Pro
+    // font is installed, we can use SF Symbols characters in place of Unicode
+    // pictographs. Other platforms do not generally have this font installed.
+    // In case rendering with SF Symbols is causing problems (e.g. a third-party
+    // terminal app is being used that doesn't support them), allow explicitly
+    // toggling them with an environment variable.
+    var useSFSymbols = false
+    if let environmentVariable = Environment.flag(named: "SWT_SF_SYMBOLS_ENABLED") {
+      useSFSymbols = environmentVariable
+    } else if useANSIEscapeCodes {
+      var statStruct = stat()
+      useSFSymbols = (0 == stat("/Library/Fonts/SF-Pro.ttf", &statStruct))
+    }
+    if useSFSymbols {
+      result.append(.useSFSymbols)
+    }
+#endif
+
+    return result
+  }
+
+  /// Whether or not the current process's standard error stream is capable of
+  /// accepting and rendering ANSI escape codes.
+  private static var _standardErrorSupportsANSIEscapeCodes: Bool {
+    // Respect the NO_COLOR environment variable. SEE: https://www.no-color.org
+    if let noColor = Environment.variable(named: "NO_COLOR"), !noColor.isEmpty {
+      return false
+    }
+
+    // Determine if stderr appears to write to a Terminal window capable of
+    // accepting ANSI escape codes.
+#if SWT_TARGET_OS_APPLE || os(Linux)
+    // If stderr is a TTY and TERM is set, that's good enough for us.
+    if 0 != isatty(STDERR_FILENO),
+       let term = Environment.variable(named: "TERM"),
+       !term.isEmpty && term != "dumb" {
+      return true
+    }
+#elseif os(Windows)
+    // If there is a console buffer associated with stderr, then it's a console.
+    if let stderrHandle = GetStdHandle(STD_ERROR_HANDLE) {
+      var screenBufferInfo = CONSOLE_SCREEN_BUFFER_INFO()
+      if GetConsoleScreenBufferInfo(stderrHandle, &screenBufferInfo) {
+        return true
+      }
+    }
+#endif
+
+    // If stderr is a pipe, assume the other end is using it to forward output
+    // from this process to its own stderr file. This is how `swift test`
+    // invokes the testing library, for example.
+#if SWT_TARGET_OS_APPLE || os(Linux)
+    var statStruct = stat()
+    if 0 == fstat(STDERR_FILENO, &statStruct) && swt_S_ISFIFO(statStruct.st_mode) {
+      return true
+    }
+#elseif os(Windows)
+    if let stderrHandle = GetStdHandle(STD_ERROR_HANDLE), FILE_TYPE_PIPE == GetFileType(stderrHandle) {
+      return true
+    }
+#endif
+
+    return false
+  }
+
+  /// Whether or not the system terminal claims to support 256-color ANSI escape
+  /// codes.
+  private static var _standardErrorSupports256ColorANSIEscapeCodes: Bool {
+#if SWT_TARGET_OS_APPLE || os(Linux)
+    if let termVariable = Environment.variable(named: "TERM") {
+      return strstr(termVariable, "256") != nil
+    }
+    return false
+#elseif os(Windows)
+    // Windows does not set the "TERM" variable, so assume it supports 256-color
+    // ANSI escape codes.
+    true
+#endif
+  }
+}
--- Sources/Testing/Running/XCTestScaffold.swift
+++ Sources/Testing/Running/XCTestScaffold.swift
@@ -168,22 +168,32 @@ public enum XCTestScaffold: Sendable {
   ///
   /// - <doc:TemporaryGettingStarted>
   public static func runAllTests(hostedBy testCase: XCTestCase) async {
-    let eventRecorder = Event.Recorder(options: .forStandardError) { string in
-      let stderr = swt_stderr()
-      fputs(string, stderr)
-      fflush(stderr)
-    }
-
+    let testCase = UncheckedSendable(rawValue: testCase)
 #if SWT_TARGET_OS_APPLE
     let isProcessLaunchedByXcode = Environment.variable(named: "XCTestSessionIdentifier") != nil
 #endif
 
-    var configuration = Configuration()
-    let testCase = UncheckedSendable(rawValue: testCase)
-    configuration.isParallelizationEnabled = false
-    configuration.eventHandler = { event, context in
-      eventRecorder.record(event, in: context)
+    // If the SWT_SELECTED_TEST_IDS environment variable is set, split it into
+    // test IDs (separated by ";", test ID components separated by "/") and set
+    // the configuration's test filter to match it.
+    //
+    // This environment variable stands in for `swift test --filter`.
+    let testIDs: [Test.ID]? = Environment.variable(named: "SWT_SELECTED_TEST_IDS").map { testIDs in
+      testIDs.split(separator: ";", omittingEmptySubsequences: true).map { testID in
+        Test.ID(testID.split(separator: "/", omittingEmptySubsequences: true).map(String.init))
+      }
+    }
+    // If the SWT_SELECTED_TAGS environment variable is set, split it by ";"
+    // (similar to test IDs above) and check if tests' tags overlap.
+    let tags: Set<Tag>? = Environment.variable(named: "SWT_SELECTED_TAGS")
+      .map { tags in
+        tags
+          .split(separator: ";", omittingEmptySubsequences: true)
+          .map(String.init)
+          .map(Tag.init(rawValue:))
+      }.map(Set.init)
 
+    await runTests(identifiedBy: testIDs, taggedWith: tags) { event, context in
       guard case let .issueRecorded(issue) = event.kind else {
         return
       }
@@ -208,140 +218,6 @@ public enum XCTestScaffold: Sendable {
       }
 #endif
     }
-
-    // If the SWT_SELECTED_TEST_IDS environment variable is set, split it into
-    // test IDs (separated by ";", test ID components separated by "/") and set
-    // the configuration's test filter to match it.
-    //
-    // This environment variable stands in for `swift test --filter`.
-    let testIDs: [Test.ID]? = Environment.variable(named: "SWT_SELECTED_TEST_IDS")
-      .map { testIDs in
-        testIDs.split(separator: ";", omittingEmptySubsequences: true).map { testID in
-          Test.ID(testID.split(separator: "/", omittingEmptySubsequences: true).map(String.init))
-        }
-      }
-    if let testIDs {
-      configuration.setTestFilter(toMatch: Set(testIDs))
-    }
-
-    // If the SWT_SELECTED_TAGS environment variable is set, split it by ";"
-    // (similar to test IDs above) and check if tests' tags overlap.
-    let tags: Set<Tag>? = Environment.variable(named: "SWT_SELECTED_TAGS")
-      .map { tags in
-        tags
-          .split(separator: ";", omittingEmptySubsequences: true)
-          .map(String.init)
-          .map(Tag.init(rawValue:))
-      }.map(Set.init)
-    if let tags {
-      // Check if the test's tags intersect the set of selected tags. If there
-      // was a previous filter function, it must also pass.
-      let oldTestFilter = configuration.testFilter ?? { _ in true }
-      configuration.testFilter = { test in
-        !tags.isDisjoint(with: test.tags) && oldTestFilter(test)
-      }
-    }
-
-    let runner = await Runner(configuration: configuration)
-    await runner.run()
   }
 }
-
-// MARK: -
-
-extension [Event.Recorder.Option] {
-  /// The set of options to use when writing to the standard error stream.
-  fileprivate static var forStandardError: Self {
-    var result = Self()
-
-    let useANSIEscapeCodes = _standardErrorSupportsANSIEscapeCodes
-    if useANSIEscapeCodes {
-      result.append(.useANSIEscapeCodes)
-      if _standardErrorSupports256ColorANSIEscapeCodes {
-        result.append(.use256ColorANSIEscapeCodes)
-      }
-    }
-
-#if os(macOS) || (os(iOS) && targetEnvironment(macCatalyst))
-    // On macOS, if we are writing to a TTY (i.e. Terminal.app) and the SF Pro
-    // font is installed, we can use SF Symbols characters in place of Unicode
-    // pictographs. Other platforms do not generally have this font installed.
-    // In case rendering with SF Symbols is causing problems (e.g. a third-party
-    // terminal app is being used that doesn't support them), allow explicitly
-    // toggling them with an environment variable.
-    var useSFSymbols = false
-    if let environmentVariable = Environment.flag(named: "SWT_SF_SYMBOLS_ENABLED") {
-      useSFSymbols = environmentVariable
-    } else if useANSIEscapeCodes {
-      var statStruct = stat()
-      useSFSymbols = (0 == stat("/Library/Fonts/SF-Pro.ttf", &statStruct))
-    }
-    if useSFSymbols {
-      result.append(.useSFSymbols)
-    }
-#endif
-
-    return result
-  }
-
-  /// Whether or not the current process's standard error stream is capable of
-  /// accepting and rendering ANSI escape codes.
-  private static var _standardErrorSupportsANSIEscapeCodes: Bool {
-    // Respect the NO_COLOR environment variable. SEE: https://www.no-color.org
-    if let noColor = Environment.variable(named: "NO_COLOR"), !noColor.isEmpty {
-      return false
-    }
-
-    // Determine if stderr appears to write to a Terminal window capable of
-    // accepting ANSI escape codes.
-#if SWT_TARGET_OS_APPLE || os(Linux)
-    // If stderr is a TTY and TERM is set, that's good enough for us.
-    if 0 != isatty(STDERR_FILENO),
-       let term = Environment.variable(named: "TERM"),
-       !term.isEmpty && term != "dumb" {
-      return true
-    }
-#elseif os(Windows)
-    // If there is a console buffer associated with stderr, then it's a console.
-    if let stderrHandle = GetStdHandle(STD_ERROR_HANDLE) {
-      var screenBufferInfo = CONSOLE_SCREEN_BUFFER_INFO()
-      if GetConsoleScreenBufferInfo(stderrHandle, &screenBufferInfo) {
-        return true
-      }
-    }
-#endif
-
-    // If stderr is a pipe, assume the other end is using it to forward output
-    // from this process to its own stderr file. This is how `swift test`
-    // invokes the testing library, for example.
-#if SWT_TARGET_OS_APPLE || os(Linux)
-    var statStruct = stat()
-    if 0 == fstat(STDERR_FILENO, &statStruct) && swt_S_ISFIFO(statStruct.st_mode) {
-      return true
-    }
-#elseif os(Windows)
-    if let stderrHandle = GetStdHandle(STD_ERROR_HANDLE), FILE_TYPE_PIPE == GetFileType(stderrHandle) {
-      return true
-    }
-#endif
-
-    return false
-  }
-
-  /// Whether or not the system terminal claims to support 256-color ANSI escape
-  /// codes.
-  private static var _standardErrorSupports256ColorANSIEscapeCodes: Bool {
-#if SWT_TARGET_OS_APPLE || os(Linux)
-    if let termVariable = Environment.variable(named: "TERM") {
-      return strstr(termVariable, "256") != nil
-    }
-    return false
-#elseif os(Windows)
-    // Windows does not set the "TERM" variable, so assume it supports 256-color
-    // ANSI escape codes.
-    true
-#endif
-  }
-}
-
 #endif
