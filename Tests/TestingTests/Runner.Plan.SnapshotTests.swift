f8f34e71567aca7d1c949a75e7c81fd968cd962c
Jonathan Grynspan <jgrynspan@apple.com>
2024-01-11 15:21:47 -0500

    Stray whitespace change

--- Tests/TestingTests/Runner.Plan.SnapshotTests.swift
+++ Tests/TestingTests/Runner.Plan.SnapshotTests.swift
@@ -7,7 +7,7 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 //
- 
+
 @testable @_spi(ExperimentalSnapshotting) @_spi(ExperimentalTestRunning) import Testing
 
 #if canImport(Foundation)
