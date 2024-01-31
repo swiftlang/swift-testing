dcfdecb658337fbb23fdd15a6fe14fd7102634e9
Jonathan Grynspan <jgrynspan@apple.com>
2024-01-31 10:07:03 -0500

    Work around a bug with variadic generics when mixed with C strings.
    There's an apparent bug (rdar://122011759) when using variadic generics and passing them a string that needs to be converted to a C string. For example:
    
    ```swift
    let file = try #require(fopen("/test/file", "wb"))
    ```
    
    The compiler generates invalid code for this call that results in the two C string arguments to `fopen()` having the same address and containing only the trailing NUL character.
    
    I have only reproduced the issue with functions imported from C; I cannot reproduce it at this time with functions declared in Swift.
    
    This change works around the issue by providing explicit overloads of `__checkFunctionCall()` for functions taking 1, 2, 3, or 4 arguments. Tests calling functions that take more arguments can be refactored, e.g.:
    
    ```swift
    let filePtr = f("a", "b", "c", "d", "e") // ...
    let file = try #require(filePtr)
    ```
    
    Resolves rdar://122014061.

---
+++ Sources/TestingInternals/include/TestSupport.h
@@ -0,0 +1,42 @@
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
+
+#if !defined(SWT_TESTSUPPORT_H)
+#define SWT_TESTSUPPORT_H
+
+/// This header includes symbols that are used by the testing library's own test
+/// targets. These symbols are not used by other clients of the testing library.
+
+#include "Defines.h"
+#include "Includes.h"
+
+SWT_ASSUME_NONNULL_BEGIN
+
+/// A type used by the testing library's own tests to validate how C
+/// enumerations are presented in test output.
+enum __attribute__((enum_extensibility(open))) SWTTestEnumeration {
+  SWTTestEnumerationA, SWTTestEnumerationB
+};
+
+static inline bool swt_pointersNotEqual2(const char *a, const char *b) {
+  return a != b;
+}
+
+static inline bool swt_pointersNotEqual3(const char *a, const char *b, const char *c) {
+  return a != b && b != c;
+}
+
+static inline bool swt_pointersNotEqual4(const char *a, const char *b, const char *c, const char *d) {
+  return a != b && b != c && c != d;
+}
+
+SWT_ASSUME_NONNULL_END
+
+#endif
