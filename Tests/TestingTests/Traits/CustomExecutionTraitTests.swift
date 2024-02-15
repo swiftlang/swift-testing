a0cb8d5817533d8293e4a8d6953d6cd3f91bdc9b
Dennis Weissmann <dennisweissmann@users.noreply.github.com>
2024-02-15 14:46:38 -0800

    Add the ability for a trait to run code before and after a test (#86)
    Add the ability for a trait to run code before and after a test.
    
    ### Motivation:
    
    As laid out in the [vision
    document](https://github.com/apple/swift-testing/blob/main/Documentation/Vision.md#trait-extensibility),
    we want traits to become extensible, and be able to invoke code *before*
    and *after* a test's body is invoked.
    
    This PR provides the foundation that will not just give test authors a
    lot more power and flexibility in what they can achieve with traits, but
    will allow us (in a future PR) to re-implement e.g. the timeout behavior
    to be less special-cased.
    
    ### Modifications:
    
    I added a new protocol `CustomExecutionTrait` which inherits from
    `Trait`, that has a single additional requirement: an `execute` function
    that provides a function that eventually executes the test body in its
    argument so that trait authors can invoke it in whatever way they need,
    ie. have the ability to execute code before and after they invoke that
    function.
    
    An example of such a trait is
    
    ```swift
    struct DoSomethingBeforeAndAfterTrait: CustomExecutionTrait & TestTrait {
    
      func execute(_ function: () async throws -> Void, for test: Test) async throws {
        something()
        try await function()
        somethingElse()
      }
    }
    ```
    
    (or see `CustomTraitTest`).
    
    ### Result:
    
    Test authors now have a much more flexible system to write traits,
    allowing them to execute custom code before and after a test body is
    run.

---
+++ Tests/TestingTests/Traits/CustomExecutionTraitTests.swift
@@ -0,0 +1,78 @@
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
+
+@testable @_spi(ExperimentalTestRunning) @_spi(ExperimentalEventHandling) @_spi(ExperimentalTraits) import Testing
+
+private struct CustomTrait: CustomExecutionTrait, TestTrait {
+    var before: Confirmation
+    var after: Confirmation
+    func execute(_ function: @escaping @Sendable () async throws -> Void, for test: Test, testCase: Test.Case?) async throws {
+        before()
+        defer {
+            after()
+        }
+        try await function()
+    }
+}
+
+private struct CustomThrowingErrorTrait: CustomExecutionTrait, TestTrait {
+    fileprivate struct CustomTraitError: Error {}
+
+    func execute(_ function: @escaping @Sendable () async throws -> Void, for test: Test, testCase: Test.Case?) async throws {
+        throw CustomTraitError()
+    }
+}
+
+struct CustomTraitTest {
+    @Test("Execute code before and after a non-parameterized test.")
+    func executeCodeBeforeAndAfterNonParameterizedTest() async {
+        // `expectedCount` is 2 because we run it both for the test and the test case
+        await confirmation("Code was run before the test", expectedCount: 2) { before in
+            await confirmation("Code was run after the test", expectedCount: 2) { after in
+                await Test(CustomTrait(before: before, after: after)) {
+                    // do nothing
+                }.run()
+            }
+        }
+    }
+
+    @Test("Execute code before and after a parameterized test.")
+    func executeCodeBeforeAndAfterParameterizedTest() async {
+        // `expectedCount` is 3 because we run it both for the test and each test case
+        await confirmation("Code was run before the test", expectedCount: 3) { before in
+            await confirmation("Code was run after the test", expectedCount: 3) { after in
+                await Test(CustomTrait(before: before, after: after), arguments: ["Hello", "World"]) { _ in
+                    // do nothing
+                }.run()
+            }
+        }
+    }
+
+    @Test("Custom execution trait throws an error")
+    func customExecutionTraitThrowsAnError() async throws {
+        var configuration = Configuration()
+        await confirmation("Error thrown", expectedCount: 1) { errorThrownConfirmation in
+            configuration.eventHandler = { event, _ in
+                guard case let .issueRecorded(issue) = event.kind,
+                      case let .errorCaught(error) = issue.kind else {
+                    return
+                }
+
+                #expect(error is CustomThrowingErrorTrait.CustomTraitError)
+                errorThrownConfirmation()
+            }
+
+            await Test(CustomThrowingErrorTrait()) {
+                // Make sure this does not get reached
+                Issue.record("Expected trait to fail the test. Should not have reached test body.")
+            }.run(configuration: configuration)
+        }
+    }
+}
