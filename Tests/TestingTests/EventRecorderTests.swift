6f5f677df0de644cb5c826f00c521035bbc2d906
Stuart Montgomery <smontgomery@apple.com>
2024-03-06 13:01:00 -0600

    Lower programmatic `.comment` trait to SPI

--- Tests/TestingTests/EventRecorderTests.swift
+++ Tests/TestingTests/EventRecorderTests.swift
@@ -1,14 +1,14 @@
 //
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Copyright (c) 2024 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 //
 
-@testable @_spi(ExperimentalEventHandling) @_spi(ExperimentalEventRecording) @_spi(ExperimentalTestRunning) import Testing
+@testable @_spi(Experimental) @_spi(ExperimentalEventHandling) @_spi(ExperimentalEventRecording) @_spi(ExperimentalTestRunning) import Testing
 #if !os(Windows)
 import RegexBuilder
 #endif
