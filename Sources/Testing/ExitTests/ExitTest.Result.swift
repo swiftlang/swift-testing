417cb17f5af129702e35542e5ada4cca36acc39d
Jonathan Grynspan <jgrynspan@apple.com>
2024-04-02 13:50:05 -0400

    [SWT-NNNN] Exit tests
    One of the first enhancement requests we received for swift-testing was the
    ability to test for precondition failures and other critical failures that
    terminate the current process when they occur. This feature is also frequently
    requested for XCTest. With swift-testing, we have the opportunity to build such
    a feature in an ergonomic way.
    
    Read the full proposal [here](https://github.com/apple/swift-testing/blob/jgrynspan/exit-tests-proposal/Documentation/Proposals/NNNN-exit-tests.md).

--- Sources/Testing/ExitTests/ExitTest.Result.swift
+++ Sources/Testing/ExitTests/ExitTest.Result.swift
@@ -8,7 +8,6 @@
 // See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 //
 
-@_spi(Experimental)
 #if SWT_NO_EXIT_TESTS
 @available(*, unavailable, message: "Exit tests are not available on this platform.")
 #endif
@@ -19,11 +18,19 @@ extension ExitTest {
   /// Both ``expect(exitsWith:observing:_:sourceLocation:performing:)`` and
   /// ``require(exitsWith:observing:_:sourceLocation:performing:)`` return
   /// instances of this type.
+  ///
+  /// @Metadata {
+  ///   @Available(Swift, introduced: 6.2)
+  /// }
   public struct Result: Sendable {
-    /// The exit condition the exit test exited with.
+    /// The status of the process hosting the exit test at the time it exits.
     ///
     /// When the exit test passes, the value of this property is equal to the
     /// exit status reported by the process that hosted the exit test.
+    ///
+    /// @Metadata {
+    ///   @Available(Swift, introduced: 6.2)
+    /// }
     public var statusAtExit: StatusAtExit
 
     /// All bytes written to the standard output stream of the exit test before
@@ -50,6 +57,10 @@ extension ExitTest {
     ///
     /// If you did not request standard output content when running an exit
     /// test, the value of this property is the empty array.
+    ///
+    /// @Metadata {
+    ///   @Available(Swift, introduced: 6.2)
+    /// }
     public var standardOutputContent: [UInt8] = []
 
     /// All bytes written to the standard error stream of the exit test before
@@ -76,6 +87,10 @@ extension ExitTest {
     ///
     /// If you did not request standard error content when running an exit test,
     /// the value of this property is the empty array.
+    ///
+    /// @Metadata {
+    ///   @Available(Swift, introduced: 6.2)
+    /// }
     public var standardErrorContent: [UInt8] = []
 
     @_spi(ForToolsIntegrationOnly)
