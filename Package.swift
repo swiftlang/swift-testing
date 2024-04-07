e0381857ae384353ee28a0f391568b2193f2c416
Jonathan Grynspan <jgrynspan@apple.com>
2024-04-07 09:57:35 -0400

    Didn't mean to change the swift-syntax dependency

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
   ],
 
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax.git", branch: "main"),
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.1"),
   ],
 
   targets: [
