c5292544ac10d498b167e557b963d241a96d4f42
Jonathan Grynspan <jgrynspan@apple.com>
2024-05-01 07:48:50 -0400

    Remove accidental specification of SWT_NO_DYNAMIC_LINKING

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -126,7 +126,6 @@ extension Array where Element == PackageDescription.SwiftSetting {
 
       .define("SWT_NO_FILE_IO", .when(platforms: [.wasi])),
       .define("SWT_NO_EXIT_TESTS", .when(platforms: [.iOS, .watchOS, .tvOS, .visionOS, .wasi])),
-      .define("SWT_NO_DYNAMIC_LINKING"),
     ]
   }
 
