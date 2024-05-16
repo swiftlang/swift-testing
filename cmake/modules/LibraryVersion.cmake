506390f766b86f7d4039d40b90f5e5be5111aa48
Jeff <jeffdav@users.noreply.github.com>
2024-05-16 09:46:06 -0700

    Remove duplicate settings.

--- cmake/modules/LibraryVersion.cmake
+++ cmake/modules/LibraryVersion.cmake
@@ -6,27 +6,6 @@
 # See http://swift.org/LICENSE.txt for license information
 # See http://swift.org/CONTRIBUTORS.txt for Swift project authors
 
-# Settings intended to be applied to every Swift target in this package.
-# Analogous to project-level build settings in an Xcode project.
-add_compile_options(
-  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -require-explicit-sendable>")
-add_compile_options(
-  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-experimental-feature -Xfrontend StrictConcurrency>"
-  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-experimental-feature -Xfrontend AccessLevelOnImport>")
-add_compile_options(
-  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-upcoming-feature -Xfrontend ExistentialAny>"
-  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-upcoming-feature -Xfrontend InternalImportsByDefault>")
-
-if(APPLE)
-  add_compile_definitions("SWT_TARGET_OS_APPLE")
-endif()
-if(WASI)
-  add_compile_definitions("SWT_NO_FILE_IO")
-endif()
-if(CMAKE_SYSTEM_NAME IN_LIST "iOS;watchOS;tvOS;visionOS;WASI")
-  add_compile_definitions("SWT_NO_EXIT_TESTS")
-endif()
-
 find_package(Git QUIET)
 if(Git_FOUND)
   execute_process(
