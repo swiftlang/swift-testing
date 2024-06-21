5eed6b33dbf53aacd4a240c0535c945430c9f9cc
Stuart Montgomery <smontgomery@apple.com>
2024-06-21 15:56:57 -0500

    Avoid linker symbol de-duplication in 'failureBreakpoint()' marker function
    Resolves rdar://129872111

--- Sources/Testing/Issues/Issue+Recording.swift
+++ Sources/Testing/Issues/Issue+Recording.swift
@@ -259,5 +259,13 @@ extension Issue {
 @inline(never) @_optimize(none)
 @usableFromInline
 func failureBreakpoint() {
-  // Empty.
+  // This function's body cannot be completely empty or else linker symbol
+  // de-duplication will cause its symbol to be consolidated with that of
+  // another, arbitrarily chosen empty function in this module. This linker
+  // behavior can be disabled by passing the `-no_deduplicate` flag described in
+  // ld(1), but that would disable it module-wide and sacrifice optimization
+  // opportunities elsewhere, so instead this function performs some trivial
+  // work to ensure its body is unique.
+  func noOp() {}
+  noOp()
 }
