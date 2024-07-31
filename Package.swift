ac83e89eaa4780b5ae72f78cd1a5475af60882d3
Yuta Saito <kateinoigakukun@gmail.com>
2024-07-31 11:24:02 +0000

    File IO functionalities used in this package are available on WASI

--- Package.swift
+++ Package.swift
@@ -130,7 +130,6 @@ extension Array where Element == PackageDescription.SwiftSetting {
 
       .define("SWT_TARGET_OS_APPLE", .when(platforms: [.macOS, .iOS, .macCatalyst, .watchOS, .tvOS, .visionOS])),
 
-      .define("SWT_NO_FILE_IO", .when(platforms: [.wasi])),
       .define("SWT_NO_EXIT_TESTS", .when(platforms: [.iOS, .watchOS, .tvOS, .visionOS, .wasi])),
       .define("SWT_NO_SNAPSHOT_TYPES", .when(platforms: [.linux, .windows, .wasi])),
       .define("SWT_NO_DYNAMIC_LINKING", .when(platforms: [.wasi])),
