b8e8d107c4af4160333c48a169c60b0b77ef23f2
Jonathan Grynspan <jgrynspan@apple.com>
2024-09-06 11:02:27 -0400

    Don't use `Synchronization.Atomic` for `deliverExpectationCheckedEvents`.
    This PR replaces the new use of `Atomic<Int>` with a `Locked<Int>` in the
    implementation of `deliverExpectationCheckedEvents`. Why? Because we're running
    into some environments where the Synchronization module isn't available (e.g.
    older host macOSes) and this is simpler. The performance profile is comparable:
    on my system, running the `repeatedlyExpect()` test takes 0.55s instead of 0.49s
    to call `#expect()` 1,000,000 times, so it's still a significant win over the
    implementation we had earlier.

--- Package.swift
+++ Package.swift
@@ -148,7 +148,6 @@ extension Array where Element == PackageDescription.SwiftSetting {
       .enableExperimentalFeature("AvailabilityMacro=_clockAPI:macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0"),
       .enableExperimentalFeature("AvailabilityMacro=_regexAPI:macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0"),
       .enableExperimentalFeature("AvailabilityMacro=_swiftVersionAPI:macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0"),
-      .enableExperimentalFeature("AvailabilityMacro=_synchronizationAPI:macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0"),
       .enableExperimentalFeature("AvailabilityMacro=_typedThrowsAPI:macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0"),
 
       .enableExperimentalFeature("AvailabilityMacro=_distantFuture:macOS 99.0, iOS 99.0, watchOS 99.0, tvOS 99.0, visionOS 99.0"),
