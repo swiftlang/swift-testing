7d34a64c67de3e7709054c34cb8fc1c12e4139da
Jonathan Grynspan <jgrynspan@apple.com>
2024-10-15 22:27:29 -0400

    Hard-code the signal list again, just in case we're messing with something internal by mistake

--- Sources/Testing/ExitTests/ExitTest.swift
+++ Sources/Testing/ExitTests/ExitTest.swift
@@ -127,7 +127,7 @@ public struct ExitTest: Sendable, ~Copyable {
     // exit code that is unlikely to be encountered "in the wild" and which
     // encodes the caught signal. Corresponding code in the parent process looks
     // for these special exit codes and translates them back to signals.
-    for sig: CInt in 0 ..< NSIG {
+    for sig in [SIGINT, SIGILL, SIGFPE, SIGSEGV, SIGTERM, SIGBREAK, SIGABRT] {
       _ = signal(sig) { sig in
         exit(_STATUS_SIGNAL_CAUGHT_BITS | sig)
       }
