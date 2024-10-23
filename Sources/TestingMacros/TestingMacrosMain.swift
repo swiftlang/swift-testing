3fa580a1ad0c5fa42d595d70ace67f83608e4b13
Jonathan Grynspan <jgrynspan@apple.com>
2024-10-23 10:44:04 -0400

    Add more test coverage, ensure that `try #require(throws: Never.self)` only throws if you try to cast the result to `any Error` (can't instantiate `Never`), add a way to suppress our diagnostics in our own unit tests.

--- Sources/TestingMacros/TestingMacrosMain.swift
+++ Sources/TestingMacros/TestingMacrosMain.swift
@@ -24,6 +24,7 @@ struct TestingMacrosMain: CompilerPlugin {
       RequireMacro.self,
       AmbiguousRequireMacro.self,
       NonOptionalRequireMacro.self,
+      RequireThrowsMacro.self,
       RequireThrowsNeverMacro.self,
       ExitTestExpectMacro.self,
       ExitTestRequireMacro.self,
