2500b5ecfac45e6be6a78aa35f7d03f709a98e35
Jonathan Grynspan <jgrynspan@apple.com>
2025-01-07 13:49:26 -0500

    Test should check the node the warning is attached to

--- Tests/TestingMacrosTests/PragmaMacroTests.swift
+++ Tests/TestingMacrosTests/PragmaMacroTests.swift
@@ -37,5 +37,6 @@ struct PragmaMacroTests {
     #expect(diagnostics.count == 1)
     #expect(diagnostics[0].message == "abc123")
     #expect(diagnostics[0].diagMessage.severity == .warning)
+    #expect(diagnostics[0].node.is(VariableDeclSyntax.self))
   }
 }
