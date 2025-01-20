334c3c7844d0d6c2ff91e420d1f5ab2a2bf7f388
Jonathan Grynspan <jgrynspan@apple.com>
2025-01-20 12:07:47 -0500

    Make all test content types directly conform to `TestContent`.
    This PR eliminates the `TestContentAccessorResult` associated type from the
    (currently internal, potentially eventually API) `TestContent` protocol. This
    associated type needed to be `~Copyable` so `ExitTest` could be used with it,
    but that appears to pose some _problems_ for the compiler (rdar://143049814&143080508).
    
    Instead, we remove the associated type and just say "the test content record is
    the type that conforms to `TestContent`". `ExitTest` is happy with this, but
    `Test`'s produced type is a non-nominal function type, so we wrap that function
    in a small private type with identical layout and have that type conform.
    
    The ultimate purpose of this PR is to get us a bit closer to turning
    `TestContent` into a public or tools-SPI protocol that other components can use
    for test discovery.

--- Sources/Testing/Test+Discovery.swift
+++ Sources/Testing/Test+Discovery.swift
@@ -10,13 +10,28 @@
 
 private import _TestingInternals
 
-extension Test: TestContent {
+/// A type that encapsulates test content records that produce instances of
+/// ``Test``.
+///
+/// This type is necessary because such test content records produce an indirect
+/// `async` accessor function rather than directly producing instances of
+/// ``Test``, but functions are non-nominal types and cannot directly conform to
+/// protocols.
+///
+/// - Note: This helper type must have the exact in-memory layout of the `async`
+///   accessor function. Do not add any additional stored properties. The layout
+///   of this type is _de facto_ [guaranteed](https://github.com/swiftlang/swift/blob/main/docs/ABI/TypeLayout.rst)
+///   by the Swift ABI.
+/* @frozen */ private struct _TestRecord: TestContent {
   static var testContentKind: UInt32 {
     0x74657374
   }
 
-  typealias TestContentAccessorResult = @Sendable () async -> Self
+  /// This instance's actual (asynchronous) accessor function.
+  var asyncAccessor: @Sendable () async -> Test
+}
 
+extension Test {
   /// All available ``Test`` instances in the process, according to the runtime.
   ///
   /// The order of values in this sequence is unspecified.
@@ -43,7 +58,7 @@ extension Test: TestContent {
       // Walk all test content and gather generator functions, then call them in
       // a task group and collate their results.
       if useNewMode {
-        let generators = Self.allTestContentRecords().lazy.compactMap { $0.load() }
+        let generators = _TestRecord.allTestContentRecords().lazy.compactMap { $0.load()?.asyncAccessor }
         await withTaskGroup(of: Self.self) { taskGroup in
           for generator in generators {
             taskGroup.addTask(operation: generator)
