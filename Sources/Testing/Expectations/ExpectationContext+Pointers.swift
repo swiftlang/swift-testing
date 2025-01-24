93e011ab60bcdd5cd6281f614ab44a0d17e32f0b
Jonathan Grynspan <jgrynspan@apple.com>
2025-01-24 16:27:39 -0500

    Work around compiler crash (rdar://143586718)

--- Sources/Testing/Expectations/ExpectationContext+Pointers.swift
+++ Sources/Testing/Expectations/ExpectationContext+Pointers.swift
@@ -32,27 +32,6 @@ extension __ExpectationContext {
     captureValue(value, id)
   }
 
-  /// Capture a pointer for use if the expectation currently being evaluated
-  /// fails.
-  ///
-  /// - Parameters:
-  ///   - value: The value to pass through.
-  ///   - id: A value that uniquely identifies the represented expression in the
-  ///     context of the expectation currently being evaluated.
-  ///
-  /// - Returns: `value`, verbatim.
-  ///
-  /// This overload of `callAsFunction()` is used when a pointer is passed to
-  /// allow for the correct handling of implicit pointer conversion after it
-  /// returns.
-  ///
-  /// - Warning: This function is used to implement the `#expect()` and
-  ///   `#require()` macros. Do not call it directly.
-  @_disfavoredOverload
-  @inlinable public mutating func callAsFunction<P>(_ value: P?, _ id: __ExpressionID) -> P? where P: _Pointer {
-    captureValue(value, id)
-  }
-
   /// Capture a string for use if the expectation currently being evaluated
   /// fails.
   ///
@@ -71,28 +50,6 @@ extension __ExpectationContext {
   /// - Warning: This function is used to implement the `#expect()` and
   ///   `#require()` macros. Do not call it directly.
   @inlinable public mutating func callAsFunction(_ value: String, _ id: __ExpressionID) -> String {
-    captureValue(value, id)
-  }
-
-  /// Capture an optional string for use if the expectation currently being
-  /// evaluated fails.
-  ///
-  /// - Parameters:
-  ///   - value: The value to pass through.
-  ///   - id: A value that uniquely identifies the represented expression in the
-  ///     context of the expectation currently being evaluated.
-  ///
-  /// - Returns: `value`, verbatim.
-  ///
-  /// This overload of `callAsFunction()` is used when an optional string is
-  /// passed to allow for the correct handling of implicit C string conversion
-  /// after it returns. For more information about implicit type conversions
-  /// performed by the Swift compiler, see [here](https://developer.apple.com/documentation/swift/calling-functions-with-pointer-parameters#Pass-a-Constant-Pointer-as-a-Parameter).
-  ///
-  /// - Warning: This function is used to implement the `#expect()` and
-  ///   `#require()` macros. Do not call it directly.
-  @_disfavoredOverload
-  @inlinable public mutating func callAsFunction(_ value: String?, _ id: __ExpressionID) -> String? {
     captureValue(value, id)
   }
 
@@ -117,7 +74,7 @@ extension __ExpectationContext {
     captureValue(value, id)
   }
 
-  /// Capture an optional array for use if the expectation currently being
+  /// Capture an optional value for use if the expectation currently being
   /// evaluated fails.
   ///
   /// - Parameters:
@@ -127,15 +84,13 @@ extension __ExpectationContext {
   ///
   /// - Returns: `value`, verbatim.
   ///
-  /// This overload of `callAsFunction()` is used when an optional array is
-  /// passed to allow for the correct handling of implicit C array conversion
-  /// after it returns. For more information about implicit type conversions
-  /// performed by the Swift compiler, see [here](https://developer.apple.com/documentation/swift/calling-functions-with-pointer-parameters#Pass-a-Constant-Pointer-as-a-Parameter).
+  /// This overload of `callAsFunction()` is used when an optional value is
+  /// passed to allow for the correct handling of various implicit conversions.
   ///
   /// - Warning: This function is used to implement the `#expect()` and
   ///   `#require()` macros. Do not call it directly.
   @_disfavoredOverload
-  @inlinable public mutating func callAsFunction<E>(_ value: Array<E>?, _ id: __ExpressionID) -> Array<E>? {
+  @inlinable public mutating func callAsFunction<T>(_ value: T?, _ id: __ExpressionID) -> T? {
     captureValue(value, id)
   }
 }
