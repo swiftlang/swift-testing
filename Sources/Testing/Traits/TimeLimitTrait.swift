c834ca10492ebf254ef4a9788e4f2c1a9ed59b42
Graham (DevPubs) Lee <glee23@apple.com>
2025-01-24 11:17:56 +0000

    Tidy up the documentation for TimeLimitTrait

--- Sources/Testing/Traits/TimeLimitTrait.swift
+++ Sources/Testing/Traits/TimeLimitTrait.swift
@@ -10,17 +10,16 @@
 
 /// A type that defines a time limit to apply to a test.
 ///
-/// To add this trait to a test, use one of the following functions:
-///
-/// - ``Trait/timeLimit(_:)-4kzjp``
+/// To add this trait to a test, use ``Trait/timeLimit(_:)-4kzjp``.
 @available(_clockAPI, *)
 public struct TimeLimitTrait: TestTrait, SuiteTrait {
   /// A type representing the duration of a time limit applied to a test.
   ///
-  /// This type is intended for use specifically for specifying test timeouts
-  /// with ``TimeLimitTrait``. It is used instead of Swift's built-in `Duration`
-  /// type because test timeouts do not support high-precision, arbitrarily
-  /// short durations. The smallest allowed unit of time is minutes.
+  /// Use this type to specify a test timeout with ``TimeLimitTrait``.
+  /// Use this type instead of Swift's built-in `Duration`
+  /// type because the testing library doesn't support high-precision,
+  /// arbitrarily short durations for test timeouts.
+  /// The smallest allowed unit of time is minutes.
   public struct Duration: Sendable {
     /// The underlying Swift `Duration` which this time limit duration
     /// represents.
@@ -29,8 +28,7 @@ public struct TimeLimitTrait: TestTrait, SuiteTrait {
     /// Construct a time limit duration given a number of minutes.
     ///
     /// - Parameters:
-    ///   - minutes: The number of minutes the resulting duration should
-    ///     represent.
+    ///   - minutes: The length of the duration in minutes.
     ///
     /// - Returns: A duration representing the specified number of minutes.
     public static func minutes(_ minutes: some BinaryInteger) -> Self {
@@ -54,8 +52,8 @@ public struct TimeLimitTrait: TestTrait, SuiteTrait {
 
 @available(_clockAPI, *)
 extension Trait where Self == TimeLimitTrait {
-  /// Construct a time limit trait that causes a test to time out if it runs for
-  /// too long.
+  /// Constructs a time limit trait that causes a test to time out if it runs
+  /// for too long.
   ///
   /// - Parameters:
   ///   - timeLimit: The maximum amount of time the test may run for.
@@ -63,26 +61,25 @@ extension Trait where Self == TimeLimitTrait {
   /// - Returns: An instance of ``TimeLimitTrait``.
   ///
   /// Test timeouts do not support high-precision, arbitrarily short durations
-  /// due to variability in testing environments. The time limit must be at
-  /// least one minute, and can only be expressed in increments of one minute.
+  /// due to variability in testing environments. You express the duration in
+  /// minutes, with a minimum duration of one minute.
   ///
-  /// When this trait is associated with a test, that test must complete within
-  /// a time limit of, at most, `timeLimit`. If the test runs longer, an issue
-  /// of kind ``Issue/Kind/timeLimitExceeded(timeLimitComponents:)`` is
-  /// recorded. This timeout is treated as a test failure.
+  /// When you associate this trait with a test, that test must complete within
+  /// a time limit of, at most, `timeLimit`. If the test runs longer, the
+  /// testing library records a
+  /// ``Issue/Kind/timeLimitExceeded(timeLimitComponents:)`` issue, which it
+  /// treats as a test failure.
   ///
-  /// The time limit amount specified by `timeLimit` may be reduced if the
-  /// testing library is configured to enforce a maximum per-test limit. When
-  /// such a maximum is set, the effective time limit of the test this trait is
-  /// applied to will be the lesser of `timeLimit` and that maximum. This is a
-  /// policy which may be configured on a global basis by the tool responsible
-  /// for launching the test process. Refer to that tool's documentation for
-  /// more details.
+  /// The testing library can use a shorter time limit than that specified by
+  /// `timeLimit` may be reduced if it's configured to enforce a maximum
+  /// per-test limit. When you configure a maximum per-test limit, the time
+  /// limit of the test this trait is applied to is the shorter of `timeLimit`
+  /// and that maximum. For information on configuring maximum per-test limits,
+  /// consult the documentation for the tool you use to run your tests.
   ///
   /// If a test is parameterized, this time limit is applied to each of its
   /// test cases individually. If a test has more than one time limit associated
-  /// with it, the shortest one is used. A test run may also be configured with
-  /// a maximum time limit per test case.
+  /// with it, the testing library uses the shortest time limit.
   @_spi(Experimental)
   public static func timeLimit(_ timeLimit: Duration) -> Self {
     return Self(timeLimit: timeLimit)
@@ -97,26 +94,25 @@ extension Trait where Self == TimeLimitTrait {
   /// - Returns: An instance of ``TimeLimitTrait``.
   ///
   /// Test timeouts do not support high-precision, arbitrarily short durations
-  /// due to variability in testing environments. The time limit must be at
-  /// least one minute, and can only be expressed in increments of one minute.
+  /// due to variability in testing environments. You express the duration in
+  /// minutes, with a minimum duration of one minute.
   ///
-  /// When this trait is associated with a test, that test must complete within
-  /// a time limit of, at most, `timeLimit`. If the test runs longer, an issue
-  /// of kind ``Issue/Kind/timeLimitExceeded(timeLimitComponents:)`` is
-  /// recorded. This timeout is treated as a test failure.
+  /// When you associate this trait with a test, that test must complete within
+  /// a time limit of, at most, `timeLimit`. If the test runs longer, the
+  /// testing library records a
+  /// ``Issue/Kind/timeLimitExceeded(timeLimitComponents:)`` issue, which it
+  /// treats as a test failure.
   ///
-  /// The time limit amount specified by `timeLimit` may be reduced if the
-  /// testing library is configured to enforce a maximum per-test limit. When
-  /// such a maximum is set, the effective time limit of the test this trait is
-  /// applied to will be the lesser of `timeLimit` and that maximum. This is a
-  /// policy which may be configured on a global basis by the tool responsible
-  /// for launching the test process. Refer to that tool's documentation for
-  /// more details.
+  /// The testing library can use a shorter time limit than that specified by
+  /// `timeLimit` may be reduced if it's configured to enforce a maximum
+  /// per-test limit. When you configure a maximum per-test limit, the time
+  /// limit of the test this trait is applied to is the shorter of `timeLimit`
+  /// and that maximum. For information on configuring maximum per-test limits,
+  /// consult the documentation for the tool you use to run your tests.
   ///
   /// If a test is parameterized, this time limit is applied to each of its
   /// test cases individually. If a test has more than one time limit associated
-  /// with it, the shortest one is used. A test run may also be configured with
-  /// a maximum time limit per test case.
+  /// with it, the testing library uses the shortest time limit.
   public static func timeLimit(_ timeLimit: Self.Duration) -> Self {
     return Self(timeLimit: timeLimit.underlyingDuration)
   }
@@ -185,11 +181,10 @@ extension TimeLimitTrait.Duration {
 
 @available(_clockAPI, *)
 extension Test {
-  /// The maximum amount of time the cases of this test may run for.
+  /// The maximum amount of time this test's cases may run for.
   ///
-  /// Time limits are associated with tests using this trait:
-  ///
-  /// - ``Trait/timeLimit(_:)-4kzjp``
+  /// Associate time limit with tests by using the ``Trait/timeLimit(_:)-4kzjp``
+  /// trait.
   ///
   /// If a test has more than one time limit associated with it, the value of
   /// this property is the shortest one. If a test has no time limits associated
@@ -201,8 +196,10 @@ extension Test {
       .min()
   }
 
-  /// Get the maximum amount of time the cases of this test may run for, taking
-  /// the current configuration and any library-imposed rules into account.
+  /// Get the maximum amount of time this test's cases may run for.
+  ///
+  /// This method uses the current configuration and any library-imposed rules,
+  /// as well as time limit traits, to calculate the time limit.
   ///
   /// - Parameters:
   ///   - configuration: The current configuration.
@@ -236,7 +233,7 @@ extension Test {
 
 // MARK: -
 
-/// An error that is reported when a test times out.
+/// An error that the testing library reports when a test times out.
 ///
 /// This type is not part of the public interface of the testing library.
 struct TimeoutError: Error, CustomStringConvertible {
@@ -259,8 +256,8 @@ struct TimeoutError: Error, CustomStringConvertible {
 /// - Throws: Any error thrown by `body`.
 ///
 /// If `body` does not return or throw before `timeLimit` is reached,
-/// `timeoutHandler` is called and given the opportunity to handle the timeout
-/// and `body` is cancelled.
+/// the function calls `timeoutHandler` and gives it the opportunity to handle
+/// the timeout, and it cancels `body`.
 ///
 /// This function is not part of the public interface of the testing library.
 @available(_clockAPI, *)
