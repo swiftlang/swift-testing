f48b44e3841a78858294301adb0b3e0610637566
Jonathan Grynspan <jgrynspan@apple.com>
2025-03-03 17:18:45 -0500

    Try to work around compiler crash

--- Sources/Testing/Running/Configuration.swift
+++ Sources/Testing/Running/Configuration.swift
@@ -217,7 +217,7 @@ public struct Configuration: Sendable {
   /// When using the `swift test` command from Swift Package Manager, this
   /// property is pre-configured. Otherwise, the default value of this property
   /// records an issue indicating that it has not been configured.
-  public var exitTestHandler: ExitTest.Handler = { _ in
+  public var exitTestHandler: ExitTest.Handler = { exitTest in
     throw SystemError(description: "Exit test support has not been implemented by the current testing infrastructure.")
   }
 #endif
