52d997b74e04e3faa7f2d347d0aa14477fd9504a
Jonathan Grynspan <jgrynspan@apple.com>
2025-03-11 11:05:21 -0400

    Remove legacy test content discovery.
    This PR removes the legacy test content discovery mechanism that is based on
    finding protocol conformances in the type metadata section emitted by the Swift
    compiler.

--- Sources/TestingMacros/ConditionMacro.swift
+++ Sources/TestingMacros/ConditionMacro.swift
@@ -12,8 +12,8 @@ public import SwiftSyntax
 import SwiftSyntaxBuilder
 public import SwiftSyntaxMacros
 
-#if !hasFeature(SymbolLinkageMarkers) && SWT_NO_LEGACY_TEST_DISCOVERY
-#error("Platform-specific misconfiguration: either SymbolLinkageMarkers or legacy test discovery is required to expand #expect(exitsWith:)")
+#if !hasFeature(SymbolLinkageMarkers)
+#error("Platform-specific misconfiguration: SymbolLinkageMarkers is required to expand #expect(exitsWith:)")
 #endif
 
 /// A protocol containing the common implementation for the expansions of the
@@ -467,19 +467,6 @@ extension ExitTestConditionMacro {
         accessingWith: .identifier("accessor")
       )
 
-      // Create another local type for legacy test discovery.
-      var recordDecl: DeclSyntax?
-#if !SWT_NO_LEGACY_TEST_DISCOVERY
-      let legacyEnumName = context.makeUniqueName("__🟡$")
-      recordDecl = """
-      enum \(legacyEnumName): Testing.__TestContentRecordContainer {
-        nonisolated static var __testContentRecord: Testing.__TestContentRecord {
-          \(enumName).testContentRecord
-        }
-      }
-      """
-#endif
-
       decls.append(
         """
         @available(*, deprecated, message: "This type is an implementation detail of the testing library. Do not use it directly.")
@@ -495,8 +482,6 @@ extension ExitTestConditionMacro {
           }
 
           \(testContentRecordDecl)
-
-          \(recordDecl)
         }
         """
       )
