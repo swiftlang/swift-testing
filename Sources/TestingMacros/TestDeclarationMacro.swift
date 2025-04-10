7780302f546b453428fdfd48fcd2c2a5dc152c0b
Jonathan Grynspan <jgrynspan@apple.com>
2025-04-10 16:37:39 -0400

    Add `unsafe` keyword handling to macro expansions.
    This PR changes the `@Test` and `#expect()` macros so they handle `unsafe`
    expressions the same way `try` and `await` expressions are handled. The
    propagation rules for `unsafe` aren't the same as for `try` and `await` (i.e. it
    doesn't colour the calling function), but the general way we handle the keyword
    is the same.
    
    I haven't attempted to avoid inserting `unsafe` if a function is not marked
    `@unsafe` as it complicates the necessary logic but has no effects at runtime.

--- Sources/TestingMacros/TestDeclarationMacro.swift
+++ Sources/TestingMacros/TestDeclarationMacro.swift
@@ -246,17 +246,17 @@ public struct TestDeclarationMacro: PeerMacro, Sendable {
     // detecting isolation to other global actors.
     lazy var isMainActorIsolated = !functionDecl.attributes(named: "MainActor", inModuleNamed: "_Concurrency").isEmpty
     var forwardCall: (ExprSyntax) -> ExprSyntax = {
-      "try await Testing.__requiringTry(Testing.__requiringAwait(\($0)))"
+      applyEffectfulKeywords([.try, .await, .unsafe], to: $0)
     }
     let forwardInit = forwardCall
     if functionDecl.noasyncAttribute != nil {
       if isMainActorIsolated {
         forwardCall = {
-          "try await MainActor.run { try Testing.__requiringTry(\($0)) }"
+          "try await MainActor.run { \(applyEffectfulKeywords([.try, .unsafe], to: $0)) }"
         }
       } else {
         forwardCall = {
-          "try { try Testing.__requiringTry(\($0)) }()"
+          "try { \(applyEffectfulKeywords([.try, .unsafe], to: $0)) }()"
         }
       }
     }
