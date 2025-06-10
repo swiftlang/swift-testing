8a33bc510cb342b2f2315ff439ee830231eec770
Jonathan Grynspan <grynspan@me.com>
2025-06-09 21:41:08 -0400

    Minor nitpicks

--- Sources/TestingMacros/ExitTestCapturedValueMacro.swift
+++ Sources/TestingMacros/ExitTestCapturedValueMacro.swift
@@ -1,18 +1,16 @@
 //
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 //
 
-import SwiftParser
 public import SwiftSyntax
 import SwiftSyntaxBuilder
 public import SwiftSyntaxMacros
-import SwiftDiagnostics
 
 /// The implementation of the `#__capturedValue()` macro when the value conforms
 /// to the necessary protocols.
