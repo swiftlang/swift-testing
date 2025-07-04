c51f8b54a9e0fb0591e6a4f96d7ccd8415c384b3
Jonathan Grynspan <jgrynspan@apple.com>
2025-07-04 07:57:14 -0400

    Sigh, instance methods

--- Sources/Testing/Events/TimeValue.swift
+++ Sources/Testing/Events/TimeValue.swift
@@ -55,7 +55,7 @@ struct TimeValue: Sendable {
   @available(_clockAPI, *)
   init(_ instant: SuspendingClock.Instant) {
 #if compiler(>=6.2)
-    self.init(SuspendingClock.systemEpoch.duration(to: instant))
+    self.init(SuspendingClock().systemEpoch.duration(to: instant))
 #else
     self.init(unsafeBitCast(instant, to: Duration.self))
 #endif
@@ -115,7 +115,7 @@ extension Duration {
 extension SuspendingClock.Instant {
   init(_ timeValue: TimeValue) {
 #if compiler(>=6.2)
-    self = SuspendingClock.systemEpoch.advanced(by: Duration(timeValue))
+    self = SuspendingClock().systemEpoch.advanced(by: Duration(timeValue))
 #else
     self = unsafeBitCast(Duration(timeValue), to: SuspendingClock.Instant.self)
 #endif
