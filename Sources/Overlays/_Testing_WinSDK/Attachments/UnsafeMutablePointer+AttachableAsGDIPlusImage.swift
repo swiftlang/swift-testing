c2e3f04cc437f5f59be19211b0c7a47019596157
Jonathan Grynspan <jgrynspan@apple.com>
2025-07-30 14:03:52 -0400

    Missing a file, sigh

---
+++ Sources/Overlays/_Testing_WinSDK/Attachments/UnsafeMutablePointer+AttachableAsGDIPlusImage.swift
@@ -0,0 +1,28 @@
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
+
+#if os(Windows)
+@_spi(Experimental) public import Testing
+
+@_spi(Experimental)
+extension UnsafeMutablePointer: AttachableAsGDIPlusImage where Pointee: AttachableAsGDIPlusImage{
+  public static func _withGDIPlusImage<A, R>(
+    at imageAddress: UnsafeMutablePointer<Self>,
+    for attachment: borrowing Attachment<_AttachableImageWrapper<A>>,
+    _ body: (OpaquePointer) throws -> R
+  ) throws -> R where A: AttachableAsGDIPlusImage {
+    try Pointee._withGDIPlusImage(at: imageAddress.pointee, for: attachment, body)
+  }
+
+  public static func _cleanUpAttachment(at imageAddress: UnsafeMutablePointer<Self>) {
+    Pointee._cleanUpAttachment(at: imageAddress.pointee)
+  }
+}
+#endif
