3a420d7c8944015746844992e442ff6be7fdc3de
Jonathan Grynspan <jgrynspan@apple.com>
2025-07-29 20:50:59 -0400

    Missing files

---
+++ Sources/Overlays/_Testing_WinSDK/Attachments/HBITMAP+AttachableAsGDIPlusImage.swift
@@ -0,0 +1,37 @@
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
+
+#if os(Windows)
+@_spi(Experimental) public import Testing
+
+// FIXME: swiftc gets confused about the _Gdiplus module using types from WinSDK; needs to be part of WinSDK directly
+public import WinSDK
+private import _Gdiplus
+
+@_spi(Experimental)
+extension WinSDK.HBITMAP__: AttachableAsGDIPlusImage {
+  public static func _withGDIPlusImage<P, R>(
+    at address: P,
+    for attachment: borrowing Attachment<some AttachableWrapper<P>>,
+    _ body: (OpaquePointer) throws -> R
+  ) throws -> R where P: _Pointer, P.Pointee == Self {
+    let address = UnsafeMutablePointer<Self>(bitPattern: UInt(bitPattern: address))
+    guard let image = swt_winsdk_GdiplusBitmapCreate(address, nil) else {
+      throw GDIPlusError.status(Gdiplus.GenericError)
+    }
+    defer {
+      swt_winsdk_GdiplusImageDelete(image)
+    }
+    return try withExtendedLifetime(self) {
+      try body(image)
+    }
+  }
+}
+#endif
---
+++ Sources/_Gdiplus/include/Stubs.h
@@ -0,0 +1,52 @@
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
+
+#if !defined(SWT_GDIPLUS_STUBS_H)
+#define SWT_GDIPLUS_STUBS_H
+
+#include "Includes.h"
+
+static inline Gdiplus::Status swt_winsdk_GdiplusStartup(
+  ULONG_PTR *token,
+  const Gdiplus::GdiplusStartupInput *input,
+  Gdiplus::GdiplusStartupOutput *output
+) {
+  return Gdiplus::GdiplusStartup(token, input, output);
+}
+
+static inline void swt_winsdk_GdiplusShutdown(ULONG_PTR token) {
+  Gdiplus::GdiplusShutdown(token);
+}
+
+static inline Gdiplus::Image *swt_winsdk_GdiplusBitmapCreate(HBITMAP bitmap, HPALETTE palette) {
+  return Gdiplus::Bitmap::FromHBITMAP(bitmap, palette);
+}
+
+static inline Gdiplus::Image *swt_winsdk_GdiplusBitmapCreate(HICON icon) {
+  return Gdiplus::Bitmap::FromHICON(icon);
+}
+
+static inline void swt_winsdk_GdiplusImageDelete(Gdiplus::Image *image) {
+  delete image;
+}
+
+static inline Gdiplus::Status swt_winsdk_GdiplusImageSave(
+  Gdiplus::Image *image,
+  IStream *stream,
+  const CLSID *format,
+  const Gdiplus::EncoderParameters *encoderParams
+) {
+  return image->Save(stream, format, encoderParams);
+}
+
+static inline void swt_winsdk_IStreamRelease(IStream *stream) {
+  stream->Release();
+}
+#endif
