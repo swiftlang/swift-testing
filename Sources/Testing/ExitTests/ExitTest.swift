c16dd29a159a2acf99fe5c73efad58c8f31e7d0a
Jonathan Grynspan <jgrynspan@apple.com>
2025-08-21 12:10:07 -0400

    Ensure attachments created in exit tests are forwarded to the parent.
    This PR ensures that when an exit test records an attachment, it is forwarded to
    the parent process.
    
    It introduces a local/private dependency on `Data` in `EncodedAttachment` so
    that we can use its base64 encoding/decoding and its ability to map files from
    disk. If Foundation is not available, it falls back to encoding/decoding
    `[UInt8]` and reading files into memory with `fread()` (the old-fashioned way).
    
    Resolves rdar://149242118.

--- Sources/Testing/ExitTests/ExitTest.swift
+++ Sources/Testing/ExitTests/ExitTest.swift
@@ -767,8 +767,12 @@ extension ExitTest {
       }
     }
     configuration.eventHandler = { event, eventContext in
-      if case .issueRecorded = event.kind {
+      switch event.kind {
+      case .issueRecorded, .valueAttached:
         eventHandler(event, eventContext)
+      default:
+        // Don't forward other kinds of event.
+        break
       }
     }
 
@@ -1034,8 +1038,11 @@ extension ExitTest {
   /// - Throws: Any error encountered attempting to decode or process the JSON.
   private static func _processRecord(_ recordJSON: UnsafeRawBufferPointer, fromBackChannel backChannel: borrowing FileHandle) throws {
     let record = try JSON.decode(ABI.Record<ABI.BackChannelVersion>.self, from: recordJSON)
+    guard case let .event(event) = record.kind else {
+      return
+    }
 
-    if case let .event(event) = record.kind, let issue = event.issue {
+    if let issue = event.issue {
       // Translate the issue back into a "real" issue and record it
       // in the parent process. This translation is, of course, lossy
       // due to the process boundary, but we make a best effort.
@@ -1063,6 +1070,8 @@ extension ExitTest {
         issueCopy.knownIssueContext = Issue.KnownIssueContext()
       }
       issueCopy.record()
+    } else if let attachment = event.attachment {
+      Attachment.record(attachment, sourceLocation: attachment._sourceLocation!)
     }
   }
 
