90853d788425ffd1f50486bebac4051d811df12b
Stuart Montgomery <smontgomery@apple.com>
2025-09-11 14:11:04 -0500

    Fix license header in files which are inconsistent

--- cmake/modules/shared/CompilerSettings.cmake
+++ cmake/modules/shared/CompilerSettings.cmake
@@ -1,10 +1,12 @@
-# This source file is part of the Swift.org open source project
-#
-# Copyright (c) 2024 Apple Inc. and the Swift project authors
-# Licensed under Apache License v2.0 with Runtime Library Exception
-#
-# See http://swift.org/LICENSE.txt for license information
-# See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+##
+## This source file is part of the Swift.org open source project
+##
+## Copyright (c) 2024 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See https://swift.org/LICENSE.txt for license information
+## See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+##
 
 # Settings intended to be applied to every Swift target in this project.
 # Analogous to project-level build settings in an Xcode project.
