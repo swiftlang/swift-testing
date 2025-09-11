77f84d3e09a44a45063daa9fb3f845757942af2c
Stuart Montgomery <smontgomery@apple.com>
2025-09-11 17:59:35 -0500

    Enable license header soundness check (#1312)

--- Tests/TestingTests/ObjCInteropTests.swift
+++ Tests/TestingTests/ObjCInteropTests.swift
@@ -1,12 +1,12 @@
-/*
- This source file is part of the Swift.org open source project
-
- Copyright (c) 2023 Apple Inc. and the Swift project authors
- Licensed under Apache License v2.0 with Runtime Library Exception
-
- See https://swift.org/LICENSE.txt for license information
- See https://swift.org/CONTRIBUTORS.txt for Swift project authors
- */
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
 
 #if canImport(XCTest)
 import XCTest
--- cmake/modules/LibraryVersion.cmake
+++ cmake/modules/LibraryVersion.cmake
@@ -1,10 +1,12 @@
-# This source file is part of the Swift.org open source project
-#
-# Copyright (c) 2024 Apple Inc. and the Swift project authors
-# Licensed under Apache License v2.0 with Runtime Library Exception
-#
-# See http://swift.org/LICENSE.txt for license information
-# See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+##
+## This source file is part of the Swift.org open source project
+##
+## Copyright (c) 2024 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See https://swift.org/LICENSE.txt for license information
+## See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+##
 
 # The current version of the Swift Testing release. For release branches,
 # remember to remove -dev.
