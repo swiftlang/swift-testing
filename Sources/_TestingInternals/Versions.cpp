442d5c80d12b910ab59d0ff0fe14a0a35929c7a6
Jonathan Grynspan <grynspan@me.com>
2025-09-17 11:36:03 -0400

    Avoid modifying the string in a constinit expression (in case that's the cause of the failure rather than #embed itself)

--- Sources/_TestingInternals/Versions.cpp
+++ Sources/_TestingInternals/Versions.cpp
@@ -13,6 +13,7 @@
 #include <array>
 #include <algorithm>
 #include <iterator>
+#include <mutex>
 
 const char *swt_getTestingLibraryVersion(void) {
 #if defined(SWT_TESTING_LIBRARY_VERSION)
@@ -20,26 +21,24 @@ const char *swt_getTestingLibraryVersion(void) {
   return SWT_TESTING_LIBRARY_VERSION;
 #elif __clang_major__ >= 17 && defined(__has_embed)
 #if __has_embed("../../VERSION.txt")
-  static constinit auto version = [] () constexpr {
-    // Read the version from version.txt at the root of the package's repo.
-    char version[] = {
+  // Read the version from version.txt at the root of the package's repo.
+  static char version[] = {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wc23-extensions"
 #embed "../../VERSION.txt"
 #pragma clang diagnostic pop
-    };
+  };
 
-    // Copy the first line from the C string into a C array so that we can
-    // return it from this closure.
-    std::array<char, std::size(version) + 1> result {};
+  // Zero out the newline character and anything after it.
+  static std::once_flag once;
+  std::call_once(once, [] {
     auto i = std::find_if(std::begin(version), std::end(version), [] (char c) {
       return c == '\r' || c == '\n';
     });
-    std::copy(std::begin(version), i, result.begin());
-    return result;
-  }();
+    std::fill(i, std::end(version), '\0');
+  });
 
-  return version.data();
+  return version;
 #else
 #warning SWT_TESTING_LIBRARY_VERSION not defined and VERSION.txt not found: testing library version is unavailable
   return nullptr;
