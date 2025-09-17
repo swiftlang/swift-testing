swiftlang/swift-testing

commit 05b46397beaf2dfad30fb853faa725fc4089cdc3

Work around lack of embed in clang on OpenBSD and Amazon Linux 2. (#1320)
Work around lack of embed in OpenBSD's clang.

### Motivation:

For various reasons, the Swift toolchain for OpenBSD relies on using the
platform's native clang, which is 16. clang 19 is the most recent
version that will not emit an error with the new __has_embed features in
C23.

Since swift-testing is experimentally supported by OpenBSD and thus to
make swift-testing build again on the platform, work around the issue
with a platform-specific command-line specified macro override in
swiftpm and in cmake.

Furthermore, we can use cmake trickery to subsitute the version file
contents instead of using embed. This may not be possible to do with
swiftpm, but I don't know for sure.

Resolves rdar://160591315.

### Checklist:

- [X] Code and documentation should follow the style of the [Style
Guide](https://github.com/apple/swift-testing/blob/main/Documentation/StyleGuide.md).
- [X] If public symbols are renamed or modified, DocC references should
be updated.

---------

Co-authored-by: Jonathan Grynspan <grynspan@me.com>
Co-authored-by: Jonathan Grynspan <jgrynspan@apple.com>

cmake/modules/shared/CompilerSettings.cmake
@@ -42,3 +42,9 @@ if(CMAKE_SYSTEM_NAME STREQUAL "WASI")
   add_compile_definitions("SWT_NO_DYNAMIC_LINKING")
   add_compile_definitions("SWT_NO_PIPES")
 endif()
+
+file(STRINGS "../VERSION.txt" SWT_TESTING_LIBRARY_VERSION LIMIT_COUNT 1)
+if(SWT_TESTING_LIBRARY_VERSION)
+  message(STATUS "Swift Testing version: ${SWT_TESTING_LIBRARY_VERSION}")
+  add_compile_definitions("$<$<COMPILE_LANGUAGE:CXX>:SWT_TESTING_LIBRARY_VERSION=\"${SWT_TESTING_LIBRARY_VERSION}\">")
+endif()

Sources/_TestingInternals/Versions.cpp
@@ -13,34 +13,42 @@
 #include <array>
 #include <algorithm>
 #include <iterator>
+#include <mutex>
 
 const char *swt_getTestingLibraryVersion(void) {
 #if defined(SWT_TESTING_LIBRARY_VERSION)
   // The current environment explicitly specifies a version string to return.
+  // All CMake builds should take this path (see CompilerSettings.cmake.)
   return SWT_TESTING_LIBRARY_VERSION;
-#elif __has_embed("../../VERSION.txt")
-  static constinit auto version = [] () constexpr {
-    // Read the version from version.txt at the root of the package's repo.
-    char version[] = {
+#elif __clang_major__ >= 17 && defined(__has_embed)
+#if __has_embed("../../VERSION.txt")
+  // Read the version from version.txt at the root of the package's repo.
+  static char version[] = {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wc23-extensions"
-#embed "../../VERSION.txt"
+#embed "../../VERSION.txt" suffix(, '\0')
 #pragma clang diagnostic pop
-    };
+  };
 
-    // Copy the first line from the C string into a C array so that we can
-    // return it from this closure.
-    std::array<char, std::size(version) + 1> result {};
+  // Zero out the newline character and anything after it.
+  static std::once_flag once;
+  std::call_once(once, [] {
     auto i = std::find_if(std::begin(version), std::end(version), [] (char c) {
       return c == '\r' || c == '\n';
     });
-    std::copy(std::begin(version), i, result.begin());
-    return result;
-  }();
+    std::fill(i, std::end(version), '\0');
+  });
 
-  return version.data();
+  return version;
 #else
 #warning SWT_TESTING_LIBRARY_VERSION not defined and VERSION.txt not found: testing library version is unavailable
+  return nullptr;
+#endif
+#elif defined(__OpenBSD__)
+  // OpenBSD's version of clang doesn't support __has_embed or #embed.
+  return nullptr;
+#else
+#warning SWT_TESTING_LIBRARY_VERSION not defined and could not read from VERSION.txt at compile time: testing library version is unavailable
   return nullptr;
 #endif
 }

commit ed0f715b61e8d09e83f7bd5a01c096c22cca0cc8

Fix the path to VERSION.txt used in CMake.
Fixes a bug introduced in #1320.

cmake/modules/shared/CompilerSettings.cmake
@@ -43,7 +43,7 @@ if(CMAKE_SYSTEM_NAME STREQUAL "WASI")
   add_compile_definitions("SWT_NO_PIPES")
 endif()
 
-file(STRINGS "../VERSION.txt" SWT_TESTING_LIBRARY_VERSION LIMIT_COUNT 1)
+file(STRINGS "${PROJECT_SOURCE_DIR}/VERSION.txt" SWT_TESTING_LIBRARY_VERSION LIMIT_COUNT 1)
 if(SWT_TESTING_LIBRARY_VERSION)
   message(STATUS "Swift Testing version: ${SWT_TESTING_LIBRARY_VERSION}")
   add_compile_definitions("$<$<COMPILE_LANGUAGE:CXX>:SWT_TESTING_LIBRARY_VERSION=\"${SWT_TESTING_LIBRARY_VERSION}\">")