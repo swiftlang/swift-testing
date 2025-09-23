635cac6acfed7286b2a1200c3e6cfe1d709e80b7
Rachel Brindle <you@subluminal.net>
2025-09-23 16:12:20 -0700

    Polling confirmations: Pass the license headers check

--- Sources/Testing/Traits/PollingConfigurationTrait.swift
+++ Sources/Testing/Traits/PollingConfigurationTrait.swift
@@ -1,8 +1,11 @@
 //
-//  PollingConfiguration.swift
-//  swift-testing
+// This source file is part of the Swift.org open source project
 //
-//  Created by Rachel Brindle on 6/6/25.
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 //
 
 /// A trait to provide a default polling configuration to all usages of
