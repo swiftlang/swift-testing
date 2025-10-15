f817ce5863fbd6ba3af57fb7837d8ef0f42336c4
Jonathan Grynspan <jgrynspan@apple.com>
2025-10-15 11:21:51 -0400

    Dynamically look up `backtrace(3)` on Android. (#1356)
    The `backtrace()` function on Android was added with API level 33, but
    at least some external Android builds target earlier Android NDKs, so
    we'll dynamically look up the function. If it's not present or dynamic
    loading isn't available, we just produce an empty backtrace.
    
    See:
    https://cs.android.com/android/_/android/platform/bionic/+/731631f300090436d7f5df80d50b6275c8c60a93:libc/include/execinfo.h;l=52
    
    Resolves #1135.
    
    ### Checklist:
    
    - [x] Code and documentation should follow the style of the [Style
    Guide](https://github.com/apple/swift-testing/blob/main/Documentation/StyleGuide.md).
    - [x] If public symbols are renamed or modified, DocC references should
    be updated.

--- Sources/Testing/SourceAttribution/Backtrace.swift
+++ Sources/Testing/SourceAttribution/Backtrace.swift
@@ -40,6 +40,16 @@ public struct Backtrace: Sendable {
     self.addresses = addresses.map { Address(UInt(bitPattern: $0)) }
   }
 
+#if os(Android) && !SWT_NO_DYNAMIC_LINKING
+  /// The `backtrace()` function.
+  ///
+  /// This function was added to Android with API level 33, which is higher than
+  /// our minimum deployment target, so we look it up dynamically at runtime.
+  private static let _backtrace = symbol(named: "backtrace").map {
+    castCFunction(at: $0, to: (@convention(c) (UnsafeMutablePointer<UnsafeMutableRawPointer?>, CInt) -> CInt).self)
+  }
+#endif
+
   /// Get the current backtrace.
   ///
   /// - Parameters:
@@ -66,9 +76,9 @@ public struct Backtrace: Sendable {
         initializedCount = .init(clamping: backtrace(addresses.baseAddress!, .init(clamping: addresses.count)))
       }
 #elseif os(Android)
-      initializedCount = addresses.withMemoryRebound(to: UnsafeMutableRawPointer.self) { addresses in
-        .init(clamping: backtrace(addresses.baseAddress!, .init(clamping: addresses.count)))
-      }
+#if !SWT_NO_DYNAMIC_LINKING
+      initializedCount = .init(clamping: _backtrace?(addresses.baseAddress!, .init(clamping: addresses.count)))
+#endif
 #elseif os(Linux) || os(FreeBSD) || os(OpenBSD)
       initializedCount = .init(clamping: backtrace(addresses.baseAddress!, .init(clamping: addresses.count)))
 #elseif os(Windows)
