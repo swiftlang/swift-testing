94e9f918977d238011215ebc5635631e80c6e01b
Jonathan Grynspan <jgrynspan@apple.com>
2025-10-16 11:57:55 -0400

    Make `Attachment` always conform to `CustomStringConvertible`.
    This PR ensures that `Attachment` conforms to `CustomStringConvertible` even if
    the value it wraps is non-copyable. We accomplish this by creating a new helper
    function that is able to box an arbitrary `~Copyable` value in `Any` if its type
    is actually `Copyable`. (This requires a newer runtime on Apple platforms, so we
    fall back to a generic (`~Copyable`) implementation on older macOS/iOS/etc.) We
    then call this helper function in `Attachment.description` and if it returns a
    value, we can stringify it.

---
+++ Sources/Testing/Support/Additions/CopyableAdditions.swift
@@ -0,0 +1,47 @@
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+//
+
+#if !hasFeature(Embedded)
+/// A helper protocol for ``boxCopyableValue(_:)``.
+private protocol _CopyablePointer {
+  /// Load the value at this address into an existential box.
+  ///
+  /// - Returns: The value at this address.
+  func load() -> Any
+}
+
+extension UnsafePointer: _CopyablePointer where Pointee: Copyable {
+  func load() -> Any {
+    pointee
+  }
+}
+#endif
+
+/// Copy a value to an existential box if its type conforms to `Copyable`.
+///
+/// - Parameters:
+///   - value: The value to copy.
+///
+/// - Returns: A copy of `value` in an existential box, or `nil` if the type of
+///   `value` does not conform to `Copyable`.
+///
+/// When using Embedded Swift, this function always returns `nil`.
+#if !hasFeature(Embedded)
+@available(_castingWithNonCopyableGenerics, *)
+func boxCopyableValue(_ value: borrowing some ~Copyable) -> Any? {
+  withUnsafePointer(to: value) { address in
+    return (address as? any _CopyablePointer)?.load()
+  }
+}
+#else
+func boxCopyableValue(_ value: borrowing some ~Copyable) -> Never? {
+  nil
+}
+#endif
