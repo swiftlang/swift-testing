swiftlang/swift-testing

commit ca6d280cc9a5c444c48c3369be3b84270686f80b

Remove `Attachable._write()`. (#1547)
This PR removes the recently-introduced experimental `_write()`
requirement on `Attachable` and replaces it with a package-only
`FileClonable` protocol that handles the clone operation. We're not
prepared to expose a `write()` method on `Attachable` at this time and
there are platform inconsistencies that make it difficult to support
file cloning with a consistent API.

### Checklist:

- [x] Code and documentation should follow the style of the [Style
Guide](https://github.com/apple/swift-testing/blob/main/Documentation/StyleGuide.md).
- [x] If public symbols are renamed or modified, DocC references should
be updated.

Sources/Overlays/_Testing_Foundation/Attachments/_AttachableURLWrapper.swift
@@ -12,7 +12,9 @@
 public import Testing
 public import Foundation
 
+#if !SWT_NO_FILE_CLONING
 private import _TestingInternals.StubsOnly
+#endif
 
 /// A wrapper type representing file system objects and URLs that can be
 /// attached indirectly.
@@ -29,12 +31,13 @@ public struct _AttachableURLWrapper: Sendable {
   /// Whether or not this instance represents a compressed directory.
   var isCompressedDirectory: Bool
 
-#if !SWT_NO_FILE_CLONING
+#if !SWT_NO_FILE_CLONING && !os(Windows)
   /// A file handle that refers to the original file (or, if a directory, the
   /// compressed copy thereof).
   ///
-  /// This file handle is used when cloning the represented file.
-  private var _fileHandle: FileHandle
+  /// This file handle is used when cloning the represented file. If the value
+  /// of this property is `nil`, cloning won't be available for said file.
+  private var _fileHandle: FileHandle?
 #endif
 
   /// Initialize an instance of this type representing a given URL.
@@ -55,8 +58,17 @@ public struct _AttachableURLWrapper: Sendable {
     self.url = url
     self.data = try Data(contentsOf: copyURL ?? url, options: [.mappedIfSafe])
     self.isCompressedDirectory = isCompressedDirectory
-#if !SWT_NO_FILE_CLONING
-    self._fileHandle = try FileHandle(forReadingFrom: copyURL ?? url)
+#if !SWT_NO_FILE_CLONING && !os(Windows)
+    self._fileHandle = (copyURL ?? url).withUnsafeFileSystemRepresentation { path in
+      guard let path else {
+        return nil
+      }
+      let fd = open(path, O_RDONLY | O_CLOEXEC)
+      guard fd >= 0 else {
+        return nil
+      }
+      return FileHandle(fileDescriptor: fd, closeOnDealloc: true)
+    }
 #endif
   }
 }
@@ -74,90 +86,6 @@ extension _AttachableURLWrapper: AttachableWrapper {
 
   public func withUnsafeBytes<R>(for attachment: borrowing Attachment<Self>, _ body: (UnsafeRawBufferPointer) throws -> R) throws -> R {
     try data.withUnsafeBytes(body)
-  }
-
-#if !SWT_NO_FILE_CLONING
-  /// Use platform-specific file-cloning API to create a copy-on-write copy of
-  /// the represented file.
-  ///
-  /// - Parameters:
-  ///   - filePath: The destination path to place the clone at.
-  ///
-  /// - Returns: Whether or not the clone operation succeeded.
-  ///
-  /// - Throws: If a file exists at `filePath`, throws `EEXIST`.
-  private func _clone(toFileAtPath filePath: String) throws -> Bool {
-    return try filePath.withCString { destinationPath throws in
-      var fileCloned = false
-
-      // Get the source file descriptor.
-#if os(Windows)
-      let srcHandle = _fileHandle._handle
-#else
-      let srcFD = _fileHandle.fileDescriptor
-#endif
-      defer {
-        extendLifetime(_fileHandle)
-      }
-
-#if SWT_TARGET_OS_APPLE
-      // Attempt to clone the source file.
-      if 0 == fclonefileat(srcFD, AT_FDCWD, destinationPath, 0) {
-        fileCloned = true
-      } else if errno == EEXIST {
-        throw POSIXError(.EEXIST)
-      }
-#elseif os(Linux) || os(FreeBSD)
-      // Open the destination file descriptor.
-      let dstFD = open(destinationPath, O_CREAT | O_EXCL | O_WRONLY | O_TRUNC, mode_t(0o666))
-      guard dstFD >= 0 else {
-        if errno == EEXIST {
-          throw POSIXError(.EEXIST)
-        }
-        return false
-      }
-      defer {
-        close(dstFD)
-      }
-
-      // Attempt to clone the source file. If the operation fails with `ENOTSUP`
-      // or `EOPNOTSUPP`, then the file system doesn't support file cloning.
-#if os(Linux)
-      fileCloned = -1 != ioctl(dstFD, swt_FICLONE(), srcFD)
-#elseif os(FreeBSD)
-      var flags = CUnsignedInt(0)
-      if getosreldate() >= 1500000 {
-        // `COPY_FILE_RANGE_CLONE` was introduced in FreeBSD 15.0, but on 14.3
-        // we can still benefit from an in-kernel copy instead.
-        flags |= swt_COPY_FILE_RANGE_CLONE()
-      }
-      fileCloned = -1 != copy_file_range(srcFD, nil, dstFD, nil, Int(SSIZE_MAX), flags)
-#endif
-      if !fileCloned {
-        // Failed to clone, but we already created the file, so we must unlink
-        // it so the fallback path works.
-        _ = unlink(destinationPath)
-      }
-#elseif os(Windows)
-      // Block cloning on Windows is only supported by ReFS which is not in
-      // wide use at this time. SEE: https://learn.microsoft.com/en-us/windows/win32/fileio/block-cloning
-      _ = srcHandle
-#else
-#warning("Platform-specific implementation missing: File cloning unavailable")
-#endif
-      return fileCloned
-    }
-  }
-#endif
-
-  public borrowing func _write(toFileAtPath filePath: String, for attachment: borrowing Attachment<Self>) throws {
-#if !SWT_NO_FILE_CLONING
-    if try _clone(toFileAtPath: filePath) {
-      return
-    }
-#endif
-    // Fall back to a byte-by-byte copy.
-    return try writeImpl(toFileAtPath: filePath, for: attachment)
   }
 
   public borrowing func preferredName(for attachment: borrowing Attachment<Self>, basedOn suggestedName: String) -> String {
@@ -185,4 +113,47 @@ extension _AttachableURLWrapper: AttachableWrapper {
     return suggestedName
   }
 }
+
+#if !SWT_NO_FILE_CLONING
+extension _AttachableURLWrapper: FileClonable {
+  public borrowing func clone(toFileAtPath filePath: String) -> Bool {
+#if SWT_TARGET_OS_APPLE || os(Linux) || os(FreeBSD)
+    guard let srcFD = _fileHandle?.fileDescriptor else {
+      return false
+    }
+#endif
+#if SWT_TARGET_OS_APPLE
+    return 0 == fclonefileat(srcFD, AT_FDCWD, filePath, 0)
+#elseif os(Linux) || os(FreeBSD)
+    // Open the destination file for exclusive writing.
+    let dstFD = open(filePath, O_CREAT | O_EXCL | O_WRONLY | O_CLOEXEC, mode_t(0o666))
+    guard dstFD >= 0 else {
+      return false
+    }
+    defer {
+      close(dstFD)
+    }
+#if os(Linux)
+    return -1 != ioctl(dstFD, swt_FICLONE(), srcFD)
+#elseif os(FreeBSD)
+    var flags = CUnsignedInt(0)
+    if Self._freeBSDVersion >= 1500000 {
+      // `COPY_FILE_RANGE_CLONE` was introduced in FreeBSD 15.0, but on 14.3
+      // we can still benefit from an in-kernel copy instead.
+      flags |= swt_COPY_FILE_RANGE_CLONE()
+    }
+    return -1 != copy_file_range(srcFD, nil, dstFD, nil, Int(SSIZE_MAX), flags)
+#endif
+#elseif os(Windows)
+    // TODO: Windows implementation
+    // Block cloning on Windows is only supported by ReFS which is not in
+    // wide use at this time. SEE: https://learn.microsoft.com/en-us/windows/win32/fileio/block-cloning
+    return false
+#else
+#warning("Platform-specific implementation missing: File cloning unavailable")
+    return false
+#endif
+  }
+}
+#endif
 #endif

Sources/Testing/Attachments/Attachment.swift
@@ -10,6 +10,10 @@
 
 private import _TestingInternals
 
+#if !SWT_NO_FILE_CLONING
+private import _TestingInternals.StubsOnly
+#endif
+
 /// A type describing values that can be attached to the output of a test run
 /// and inspected later by the user.
 ///
@@ -132,9 +136,6 @@ public struct AnyAttachable: AttachableWrapper, Sendable, Copyable {
   init<A>(_ attachment: Attachment<A>) where A: Attachable & Sendable & ~Copyable {
     _estimatedAttachmentByteCount = { attachment.attachableValue.estimatedAttachmentByteCount }
     _withUnsafeBytes = { try attachment.withUnsafeBytes($0) }
-#if !SWT_NO_FILE_IO
-    _writeToFileAtPath = { try attachment.attachableValue._write(toFileAtPath: $0, for: attachment) }
-#endif
     _preferredName = { attachment.attachableValue.preferredName(for: attachment, basedOn: $0) }
   }
 
@@ -157,16 +158,6 @@ public struct AnyAttachable: AttachableWrapper, Sendable, Copyable {
     }
     return result
   }
-
-#if !SWT_NO_FILE_IO
-  /// The implementation of `_write(toFileAtPath:for:)` borrowed from the
-  /// original attachment.
-  private var _writeToFileAtPath: @Sendable (String) throws -> Void
-
-  public borrowing func _write(toFileAtPath filePath: String, for attachment: borrowing Attachment<Self>) throws {
-    try _writeToFileAtPath(filePath)
-  }
-#endif
 
   /// The implementation of ``preferredName(for:basedOn:)`` borrowed from the
   /// original attachment.
@@ -384,6 +375,38 @@ extension Attachment where AttachableValue: ~Copyable {
 #if !SWT_NO_FILE_IO
 // MARK: - Writing
 
+extension Attachable where Self: ~Copyable {
+  /// Write this instance to the given file system path.
+  ///
+  /// - Parameters:
+  ///   - filePath: The path to write to.
+  ///   - attachment: The attachment that is requesting this instance be written
+  ///     (that is, the attachment containing this instance.)
+  ///
+  /// - Throws: Any error that prevented writing this instance to `filePath`.
+  ///
+  /// The testing library uses this function when saving an attachment. The
+  /// default implementation opens `filePath` for writing with exclusive access,
+  /// then passes it to `_write(toFILE:for:)`.
+  borrowing func write(toFileAtPath filePath: String, for attachment: borrowing Attachment<Self>) throws {
+#if !SWT_NO_FILE_CLONING
+    if #available(_castingWithNonCopyableGenerics, *),
+       let self = makeExistential(self) as? any FileClonable,
+       self.clone(toFileAtPath: filePath) {
+      return
+    }
+#endif
+
+    try withUnsafeBytes(for: attachment) { buffer in
+      // Note "x" in the mode string which indicates that the file should be
+      // created and opened exclusively. The underlying `fopen()` call will thus
+      // fail with `EEXIST` if a file exists at `filePath`.
+      let file = try FileHandle(atPath: filePath, mode: "wxeb")
+      try file.write(buffer)
+    }
+  }
+}
+
 extension Attachment where AttachableValue: ~Copyable {
   /// Write the attachment's contents to a file in the specified directory.
   ///
@@ -447,7 +470,7 @@ extension Attachment where AttachableValue: ~Copyable {
       // file exists at this path, an error with code `EEXIST` will be thrown
       // and we'll try again by adding a suffix.
       let preferredPath = appendPathComponent(preferredName, to: directoryPath)
-      try attachableValue._write(toFileAtPath: preferredPath, for: self)
+      try attachableValue.write(toFileAtPath: preferredPath, for: self)
       result = preferredPath
     } catch {
       // Split the extension(s) off the preferred name. The first component in
@@ -463,7 +486,7 @@ extension Attachment where AttachableValue: ~Copyable {
         // Propagate any error *except* EEXIST, which would indicate that the
         // name was already in use (so we should try again with a new suffix.)
         do {
-          try attachableValue._write(toFileAtPath: preferredPath, for: self)
+          try attachableValue.write(toFileAtPath: preferredPath, for: self)
           result = preferredPath
           break
         } catch where error._code == swt_EEXIST() && error._domain == "NSPOSIXErrorDomain" {

Sources/Testing/Attachments/Attachable.swift
@@ -80,30 +80,6 @@ public protocol Attachable: ~Copyable {
   /// }
   borrowing func withUnsafeBytes<R>(for attachment: borrowing Attachment<Self>, _ body: (UnsafeRawBufferPointer) throws -> R) throws -> R
 
-#if !SWT_NO_FILE_IO
-  /// Write this instance to the given file system path.
-  ///
-  /// - Parameters:
-  ///   - filePath: The path to write to.
-  ///   - attachment: The attachment that is requesting this instance be written
-  ///     (that is, the attachment containing this instance.)
-  ///
-  /// - Throws: Any error that prevented writing this instance to `filePath`.
-  ///   When the testing library calls this function, this function throws an
-  ///   error of type [`POSIXError`](https://developer.apple.com/documentation/foundation/posixerror),
-  ///   and that error's code equals [`EEXIST`](https://developer.apple.com/documentation/foundation/posixerror/eexist),
-  ///   the testing library tries again with a new path.
-  ///
-  /// The testing library uses this function when saving an attachment. The
-  /// default implementation opens `filePath` for writing, calls
-  /// ``withUnsafeBytes(for:_:)``, and writes the resulting buffer to the opened
-  /// file.
-  ///
-  /// - Warning: This function is not part of the testing library's public
-  ///   interface. It may be removed in a future update.
-  borrowing func _write(toFileAtPath filePath: String, for attachment: borrowing Attachment<Self>) throws
-#endif
-
   /// Generate a preferred name for the given attachment.
   ///
   /// - Parameters:
@@ -125,6 +101,31 @@ public protocol Attachable: ~Copyable {
   borrowing func preferredName(for attachment: borrowing Attachment<Self>, basedOn suggestedName: String) -> String
 }
 
+// MARK: - FileClonable
+
+#if !SWT_NO_FILE_CLONING
+/// A protocol describing a type whose instances represent files and can be
+/// cloned (on platforms that support file cloning).
+///
+/// This protocol is not part of the public interface of the testing library. We
+/// may wish to promote it to API in the future if there is a need for it, but
+/// our Foundation overlay probably covers the real-world use cases.
+package protocol FileClonable {
+  /// Clone the file this instance represents to the given file path.
+  ///
+  /// - Parameters:
+  ///   - filePath: The path to clone this instance to.
+  ///
+  /// - Returns: Whether or not the file was successfully cloned.
+  ///
+  /// The implementation must not attempt to write to `filePath` if it already
+  /// exists. The testing library cannot check if a file exists at `filePath`
+  /// before calling this function (a race condition would be present if it did
+  /// attempt to check first).
+  borrowing func clone(toFileAtPath filePath: String) -> Bool
+}
+#endif
+
 // MARK: - Default implementations
 
 /// @Metadata {
@@ -139,26 +140,6 @@ extension Attachable where Self: ~Copyable {
   public var estimatedAttachmentByteCount: Int? {
     nil
   }
-
-#if !SWT_NO_FILE_IO
-  /// The shared implementation of `_write(toFileAtPath:for:)` used by
-  /// attachable types declared in the testing library.
-  ///
-  /// For documentation, see `Attachable/_write(toFileAtPath:for:)`.
-  package borrowing func writeImpl(toFileAtPath filePath: String, for attachment: borrowing Attachment<Self>) throws {
-    try withUnsafeBytes(for: attachment) { buffer in
-      // Note "x" in the mode string which indicates that the file should be
-      // created and opened exclusively. The underlying `fopen()` call will thus
-      // fail with `EEXIST` if a file exists at `filePath`.
-      let file = try FileHandle(atPath: filePath, mode: "wxeb")
-      try file.write(buffer)
-    }
-  }
-
-  public borrowing func _write(toFileAtPath filePath: String, for attachment: borrowing Attachment<Self>) throws {
-    try writeImpl(toFileAtPath: filePath, for: attachment)
-  }
-#endif
 
   /// @Metadata {
   ///   @Available(Swift, introduced: 6.2)

commit 2d65aa6bce56094517383cfec4a41bc7b2ca71bd

Fix file cloning in attachments broken by #1547. (#1552)
I broke file cloning in #1547, but I did it in a bunch of different ways
that mostly cancelled each other out, so tests ended up passing
spuriously.

### Checklist:

- [x] Code and documentation should follow the style of the [Style
Guide](https://github.com/apple/swift-testing/blob/main/Documentation/StyleGuide.md).
- [x] If public symbols are renamed or modified, DocC references should
be updated.

Sources/Overlays/_Testing_Foundation/Attachments/_AttachableURLWrapper.swift
@@ -116,6 +116,11 @@ extension _AttachableURLWrapper: AttachableWrapper {
 
 #if !SWT_NO_FILE_CLONING
 extension _AttachableURLWrapper: FileClonable {
+#if os(FreeBSD)
+  /// An integer encoding the FreeBSD version number.
+  private static let _freeBSDVersion = getosreldate()
+#endif
+
   public borrowing func clone(toFileAtPath filePath: String) -> Bool {
 #if SWT_TARGET_OS_APPLE || os(Linux) || os(FreeBSD)
     guard let srcFD = _fileHandle?.fileDescriptor else {
@@ -134,7 +139,7 @@ extension _AttachableURLWrapper: FileClonable {
       close(dstFD)
     }
 #if os(Linux)
-    return -1 != ioctl(dstFD, swt_FICLONE(), srcFD)
+    let fileCloned = -1 != ioctl(dstFD, swt_FICLONE(), srcFD)
 #elseif os(FreeBSD)
     var flags = CUnsignedInt(0)
     if Self._freeBSDVersion >= 1500000 {
@@ -142,8 +147,14 @@ extension _AttachableURLWrapper: FileClonable {
       // we can still benefit from an in-kernel copy instead.
       flags |= swt_COPY_FILE_RANGE_CLONE()
     }
-    return -1 != copy_file_range(srcFD, nil, dstFD, nil, Int(SSIZE_MAX), flags)
+    let fileCloned = -1 != copy_file_range(srcFD, nil, dstFD, nil, Int(SSIZE_MAX), flags)
 #endif
+    if !fileCloned {
+      // Failed to clone, but we already created the file, so we must unlink it
+      // so the fallback path works.
+      _ = unlink(filePath)
+    }
+    return fileCloned
 #elseif os(Windows)
     // TODO: Windows implementation
     // Block cloning on Windows is only supported by ReFS which is not in

Sources/Testing/Attachments/Attachment.swift
@@ -137,6 +137,9 @@ public struct AnyAttachable: AttachableWrapper, Sendable, Copyable {
     _estimatedAttachmentByteCount = { attachment.attachableValue.estimatedAttachmentByteCount }
     _withUnsafeBytes = { try attachment.withUnsafeBytes($0) }
     _preferredName = { attachment.attachableValue.preferredName(for: attachment, basedOn: $0) }
+#if !SWT_NO_FILE_CLONING
+    _clone = { attachment.attachableValue.clone(toFileAtPath: $0, for: attachment) }
+#endif
   }
 
   /// The implementation of ``estimatedAttachmentByteCount`` borrowed from the
@@ -166,7 +169,21 @@ public struct AnyAttachable: AttachableWrapper, Sendable, Copyable {
   public borrowing func preferredName(for attachment: borrowing Attachment<Self>, basedOn suggestedName: String) -> String {
     _preferredName(suggestedName)
   }
+
+#if !SWT_NO_FILE_CLONING
+  /// The implementation of ``clone(toFileAtPath:)`` borrowed from the original
+  /// attachment.
+  private var _clone: @Sendable (String) -> Bool
+
+  package func clone(toFileAtPath filePath: String) -> Bool {
+    _clone(filePath)
+  }
+#endif
 }
+
+#if !SWT_NO_FILE_CLONING
+extension AnyAttachable: FileClonable {}
+#endif
 
 // MARK: - Describing an attachment
 
@@ -376,6 +393,25 @@ extension Attachment where AttachableValue: ~Copyable {
 // MARK: - Writing
 
 extension Attachable where Self: ~Copyable {
+#if !SWT_NO_FILE_CLONING
+  /// Clone the file this instance represents to the given file path if it
+  /// conforms to ``FileClonable``.
+  ///
+  /// - Parameters:
+  ///   - filePath: The path to clone this instance to.
+  ///
+  /// - Returns: Whether or not the file was successfully cloned.
+  ///
+  /// See ``FileClonable`` for more information.
+  fileprivate func clone(toFileAtPath filePath: String, for attachment: borrowing Attachment<Self>) -> Bool {
+    if #available(_castingWithNonCopyableGenerics, *),
+       let self = makeExistential(self) as? any FileClonable {
+      return self.clone(toFileAtPath: filePath)
+    }
+    return false
+  }
+#endif
+
   /// Write this instance to the given file system path.
   ///
   /// - Parameters:
@@ -390,9 +426,7 @@ extension Attachable where Self: ~Copyable {
   /// then passes it to `_write(toFILE:for:)`.
   borrowing func write(toFileAtPath filePath: String, for attachment: borrowing Attachment<Self>) throws {
 #if !SWT_NO_FILE_CLONING
-    if #available(_castingWithNonCopyableGenerics, *),
-       let self = makeExistential(self) as? any FileClonable,
-       self.clone(toFileAtPath: filePath) {
+    if clone(toFileAtPath: filePath, for: attachment) {
       return
     }
 #endif

Tests/TestingTests/AttachmentTests.swift
@@ -202,6 +202,31 @@ struct AttachmentTests {
   }
 #endif
 
+  @Test func cloneAttachment() async throws {
+    struct MyFileClonable: Attachable, FileClonable {
+      var withUnsafeBytesCalled: Confirmation
+
+      func withUnsafeBytes<R>(for attachment: borrowing Attachment<Self>, _ body: (UnsafeRawBufferPointer) throws -> R) throws -> R {
+        withUnsafeBytesCalled()
+        return try Array<UInt8>().withUnsafeBytes(body)
+      }
+
+      var cloneCalled: Confirmation
+
+      func clone(toFileAtPath filePath: String) -> Bool {
+        cloneCalled()
+        return true // don't actually write anything
+      }
+    }
+
+    try await confirmation("withUnsafeBytes(for:_:) called", expectedCount: 0) { withUnsafeBytesCalled in
+      try await confirmation("clone(toFileAtPath:) called") { cloneCalled in
+        let attachableValue = MyFileClonable(withUnsafeBytesCalled: withUnsafeBytesCalled, cloneCalled: cloneCalled)
+        _ = try Attachment(attachableValue).write(toFileInDirectoryAtPath: "/not/real/directory/")
+      }
+    }
+  }
+
   @Test func attachValue() async {
     await confirmation("Attachment detected", expectedCount: 2) { valueAttached in
       var configuration = Configuration()