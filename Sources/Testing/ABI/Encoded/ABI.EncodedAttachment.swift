69496862dfd15ddf3cf4bc34535f5d9737208ab4
Jonathan Grynspan <jgrynspan@apple.com>
2026-02-24 15:13:29 -0500

    Make EncodedAttachment lazily encode the attachment to avoid serialization on code paths that don't end up using JSON

--- Sources/Testing/ABI/Encoded/ABI.EncodedAttachment.swift
+++ Sources/Testing/ABI/Encoded/ABI.EncodedAttachment.swift
@@ -20,14 +20,53 @@ extension ABI {
   /// assists in converting values to JSON; clients that consume this JSON are
   /// expected to write their own decoders.
   struct EncodedAttachment<V>: Sendable where V: ABI.Version {
-    /// The path where the attachment was written.
-    var path: String?
+    /// The different kinds of encoded attachment.
+    fileprivate enum Kind: Sendable {
+      /// The attachment has already been saved to disk and we have its local
+      /// file system path.
+      case savedAtPath(String)
+
+      /// The attachment is stored in memory and we have its serialized form.
+      case inMemory(Bytes)
+
+      /// The attachment has not been saved nor serialized yet and we still have
+      /// it as an attachable value.
+      case abstract(Attachment<AnyAttachable>)
+    }
+
+    /// The kind of encoded attachment.
+    fileprivate var kind: Kind
 
     /// The preferred name of the attachment.
     ///
     /// - Warning: Attachments' preferred names are not yet part of the JSON
     ///   schema.
     var _preferredName: String?
+
+    init(encoding attachment: borrowing Attachment<AnyAttachable>, in eventContext: borrowing Event.Context) {
+      if let path = attachment.fileSystemPath {
+        kind = .savedAtPath(path)
+      } else {
+        kind = .abstract(copy attachment)
+      }
+
+      if V.includesExperimentalFields {
+        _preferredName = attachment.preferredName
+      }
+    }
+
+    /// The path where the attachment was written.
+    var path: String? {
+      if case let .savedAtPath(path) = kind {
+        return path
+      }
+      return nil
+    }
+
+    /// A structure representing the bytes of an attachment.
+    struct Bytes: Sendable, RawRepresentable {
+      var rawValue: [UInt8]
+    }
 
     /// The raw content of the attachment, if available.
     ///
@@ -36,32 +75,58 @@ extension ABI {
     /// the original attachment's serialized representation.
     ///
     /// - Warning: Inline attachment content is not yet part of the JSON schema.
-    var _bytes: Bytes?
-
-    init(encoding attachment: borrowing Attachment<AnyAttachable>, in eventContext: borrowing Event.Context) {
-      path = attachment.fileSystemPath
-
-      if V.includesExperimentalFields {
-        _preferredName = attachment.preferredName
-
-        if path == nil {
-          _bytes = try? attachment.withUnsafeBytes { bytes in
-            return Bytes(rawValue: [UInt8](bytes))
-          }
-        }
+    var _bytes: Bytes? {
+      switch kind {
+      case let .inMemory(bytes):
+        return bytes
+      case let .abstract(attachment):
+        return try? attachment.withUnsafeBytes { Bytes(rawValue: Array($0)) }
+      default:
+        return nil
       }
-    }
-
-    /// A structure representing the bytes of an attachment.
-    struct Bytes: Sendable, RawRepresentable {
-      var rawValue: [UInt8]
     }
   }
 }
 
 // MARK: - Codable
 
-extension ABI.EncodedAttachment: Codable {}
+extension ABI.EncodedAttachment: Codable {
+  private enum CodingKeys: String, CodingKey {
+  case path
+  case preferredName = "_preferredName"
+  case bytes = "_bytes"
+  }
+
+  func encode(to encoder: any Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encodeIfPresent(path, forKey: .path)
+    if V.includesExperimentalFields {
+      try container.encodeIfPresent(_bytes, forKey: .bytes)
+      try container.encodeIfPresent(_preferredName, forKey: .preferredName)
+    }
+  }
+
+  init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    if let path = try container.decodeIfPresent(String.self, forKey: .path) {
+      kind = .savedAtPath(path)
+    } else if V.includesExperimentalFields,
+              let bytes = try container.decodeIfPresent(Bytes.self, forKey: .bytes) {
+      kind = .inMemory(bytes)
+    } else {
+      throw DecodingError.valueNotFound(
+        String.self,
+        DecodingError.Context(
+          codingPath: decoder.codingPath + [CodingKeys.path],
+          debugDescription: "Encoded attachment did not include any persistent representation."
+        )
+      )
+    }
+    if V.includesExperimentalFields {
+      _preferredName = try container.decodeIfPresent(String.self, forKey: .preferredName)
+    }
+  }
+}
 
 extension ABI.EncodedAttachment.Bytes: Codable {
   func encode(to encoder: any Encoder) throws {
@@ -100,7 +165,14 @@ extension ABI.EncodedAttachment.Bytes: Codable {
 
 extension ABI.EncodedAttachment: Attachable {
   var estimatedAttachmentByteCount: Int? {
-    _bytes?.rawValue.count
+    switch kind {
+    case .savedAtPath:
+      return nil
+    case let .inMemory(bytes):
+      return bytes.rawValue.count
+    case let .abstract(attachment):
+      return attachment.attachableValue.estimatedAttachmentByteCount
+    }
   }
 
   /// An error type that is thrown when ``ABI/EncodedAttachment`` cannot satisfy
@@ -108,27 +180,27 @@ extension ABI.EncodedAttachment: Attachable {
   fileprivate struct BytesUnavailableError: Error {}
 
   borrowing func withUnsafeBytes<R>(for attachment: borrowing Attachment<Self>, _ body: (UnsafeRawBufferPointer) throws -> R) throws -> R {
-    if let bytes = _bytes?.rawValue {
-      return try bytes.withUnsafeBytes(body)
-    }
-
+    switch kind {
+    case let .savedAtPath(path):
 #if !SWT_NO_FILE_IO
-    guard let path else {
-      throw BytesUnavailableError()
-    }
 #if canImport(Foundation)
-    // Leverage Foundation's file-mapping logic since we're using Data anyway.
-    let url = URL(fileURLWithPath: path, isDirectory: false)
-    let bytes = try Data(contentsOf: url, options: [.mappedIfSafe])
+      // Leverage Foundation's file-mapping logic since we're using Data anyway.
+      let url = URL(fileURLWithPath: path, isDirectory: false)
+      let bytes = try Data(contentsOf: url, options: [.mappedIfSafe])
 #else
-    let fileHandle = try FileHandle(forReadingAtPath: path)
-    let bytes = try fileHandle.readToEnd()
+      let fileHandle = try FileHandle(forReadingAtPath: path)
+      let bytes = try fileHandle.readToEnd()
 #endif
-    return try bytes.withUnsafeBytes(body)
+      return try bytes.withUnsafeBytes(body)
 #else
-    // Cannot read the attachment from disk on this platform.
-    throw BytesUnavailableError()
+      // Cannot read the attachment from disk on this platform.
+      throw BytesUnavailableError()
 #endif
+    case let .inMemory(bytes):
+      return try bytes.rawValue.withUnsafeBytes(body)
+    case let .abstract(attachment):
+      return try attachment.withUnsafeBytes(body)
+    }
   }
 
   borrowing func preferredName(for attachment: borrowing Attachment<Self>, basedOn suggestedName: String) -> String {
