541327b3751ccdf90d0f27c732ecd9013c68f4f0
Jonathan Grynspan <jgrynspan@apple.com>
2026-02-25 17:21:47 -0500

    That shouldn't be nonsending

--- Sources/TestingMacros/TestDeclarationMacro.swift
+++ Sources/TestingMacros/TestDeclarationMacro.swift
@@ -456,7 +456,7 @@ public struct TestDeclarationMacro: PeerMacro, Sendable {
       """
       @available(*, deprecated, message: "This type is an implementation detail of the testing library. Do not use it directly.")
       enum \(enumName): Testing.__TestContentRecordContainer {
-        nonisolated(nonsending) static var __testContentRecord: Testing.__TestContentRecord6_2 {
+        nonisolated static var __testContentRecord: Testing.__TestContentRecord6_2 {
           unsafe \(testContentRecordName)
         }
       }
