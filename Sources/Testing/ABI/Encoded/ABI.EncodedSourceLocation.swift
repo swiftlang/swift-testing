216b54a792f26ae90a8f8abd01d64f4ec07fcc68
Jonathan Grynspan <jgrynspan@apple.com>
2026-02-25 10:26:15 -0500

    Expose `ABI.EncodedSourceLocation` as tools SPI.
    Follow-up to #1587.
    
    Exposes `ABI.EncodedSourceLocation` as SPI along with some conversion helpers to
    go to/from a `SourceLocation` instance. A value of this type is associated with
    many events in the event stream.

--- Sources/Testing/ABI/Encoded/ABI.EncodedSourceLocation.swift
+++ Sources/Testing/ABI/Encoded/ABI.EncodedSourceLocation.swift
@@ -15,52 +15,93 @@ extension ABI {
   /// This type is not part of the public interface of the testing library. It
   /// assists in converting values to JSON; clients that consume this JSON are
   /// expected to write their own decoders.
-  struct EncodedSourceLocation<V>: Sendable where V: ABI.Version {
-    /// See ``SourceLocation`` for a discussion of these properties.
-    var fileID: String?
-    var filePath: String?
-    var _filePath: String?
-    var line: Int
-    var column: Int
+  public struct EncodedSourceLocation<V>: Sendable where V: ABI.Version {
+    /// The file ID of the source file.
+    public var fileID: String?
 
-    init(encoding sourceLocation: borrowing SourceLocation) {
+    /// The file path of the source file.
+    public var filePath: String? {
+      get {
+        _filePath_v6_3 ?? _filePath_v0
+      }
+      set {
+        // When using the 6.3 schema, we encode both "filePath" and "_filePath"
+        // to ease migration for existing tools.
+        if V.versionNumber >= ABI.v6_3.versionNumber {
+          _filePath_v6_3 = newValue
+        }
+        if V.versionNumber <= ABI.v6_3.versionNumber {
+          _filePath_v0 = newValue
+        }
+      }
+    }
+
+    /// The line in the source file.
+    public var line: Int = 1
+
+    /// The column in the source file.
+    public var column: Int = 1
+
+    /// Storage for ``filePath`` under the `"_filePath"` JSON key, as used prior
+    /// to Swift 6.3.
+    private var _filePath_v0: String?
+
+    /// Storage for ``filePath`` under the `"filePath"` JSON key, as used in
+    /// Swift 6.3 and newer.
+    private var _filePath_v6_3: String?
+
+    public init(encoding sourceLocation: borrowing SourceLocation) {
       fileID = sourceLocation.fileID
+      filePath = sourceLocation.filePath
+      line = sourceLocation.line
+      column = sourceLocation.column
 
       // When using the 6.3 schema, don't encode synthesized file IDs.
       if V.versionNumber >= ABI.v6_3.versionNumber,
          sourceLocation.moduleName == SourceLocation.synthesizedModuleName {
         fileID = nil
       }
-
-      // When using the 6.3 schema, we encode both "filePath" and "_filePath" to
-      // ease migration for existing tools.
-      if V.versionNumber >= ABI.v6_3.versionNumber {
-        filePath = sourceLocation.filePath
-      }
-      if V.versionNumber <= ABI.v6_3.versionNumber {
-        _filePath = sourceLocation.filePath
-      }
-
-      line = sourceLocation.line
-      column = sourceLocation.column
     }
   }
 }
 
 // MARK: - Codable
 
-extension ABI.EncodedSourceLocation: Codable {}
+extension ABI.EncodedSourceLocation: Codable {
+  private enum CodingKeys: String, CodingKey {
+    case fileID
+    case _filePath_v0 = "_filePath"
+    case _filePath_v6_3 = "filePath"
+    case line
+    case column
+  }
+}
 
 // MARK: -
 
+@_spi(ForToolsIntegrationOnly)
 extension SourceLocation {
-  init?<V>(_ sourceLocation: ABI.EncodedSourceLocation<V>) {
+  /// Initialize an instance of this type from the given value.
+  ///
+  /// - Parameters:
+  ///   - sourceLocation: The encoded source location to initialize this
+  ///     instance from.
+  ///
+  /// If `sourceLocation` does not specify a value for its `fileID` field, the
+  /// testing library synthesizes a value from its `filePath` property and
+  /// hard-codes a module name of `"__C"`.
+  public init?<V>(_ sourceLocation: ABI.EncodedSourceLocation<V>) {
     let fileID = sourceLocation.fileID
-    guard let filePath = sourceLocation.filePath ?? sourceLocation._filePath else {
-      return nil
-    }
+    let filePath = sourceLocation.filePath
     let line = max(1, sourceLocation.line)
     let column = max(1, sourceLocation.column)
+
+    if let fileID, !fileID.utf8.contains(UInt8(ascii: "/")) {
+      return nil
+    }
+    guard let filePath else {
+      return nil
+    }
 
     self.init(fileIDSynthesizingIfNeeded: fileID, filePath: filePath, line: line, column: column)
   }
