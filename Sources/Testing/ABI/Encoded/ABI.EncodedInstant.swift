124669eda79e5550caf4a44b1f0e08b12a6fb6f5
Jonathan Grynspan <jgrynspan@apple.com>
2026-03-05 13:51:01 -0500

    Expose `ABI.EncodedInstant` as tools SPI. (#1588)
    Follow-up to #1587.
    
    Exposes `ABI.EncodedInstant` as SPI along with some conversion helpers
    to get a Foundation `Date` or suspending clock instant from an instance
    thereof. A value of this type is associated with each event in the event
    stream.
    
    ### Checklist:
    
    - [x] Code and documentation should follow the style of the [Style
    Guide](https://github.com/apple/swift-testing/blob/main/Documentation/StyleGuide.md).
    - [x] If public symbols are renamed or modified, DocC references should
    be updated.

--- Sources/Testing/ABI/Encoded/ABI.EncodedInstant.swift
+++ Sources/Testing/ABI/Encoded/ABI.EncodedInstant.swift
@@ -1,7 +1,7 @@
 //
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2024 Apple Inc. and the Swift project authors
+// Copyright (c) 2024–2026 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
@@ -12,28 +12,53 @@ extension ABI {
   /// A type implementing the JSON encoding of ``Test/Clock/Instant`` for the
   /// ABI entry point and event stream output.
   ///
-  /// This type is not part of the public interface of the testing library. It
-  /// assists in converting values to JSON; clients that consume this JSON are
-  /// expected to write their own decoders.
-  struct EncodedInstant<V>: Sendable where V: ABI.Version {
+  /// You can use this type and its conformance to [`Codable`](https://developer.apple.com/documentation/swift/codable),
+  /// when integrating the testing library with development tools. It is not
+  /// part of the testing library's public interface.
+  public struct EncodedInstant<V>: Sendable where V: ABI.Version {
     /// The number of seconds since the system-defined suspending epoch.
     ///
     /// For more information, see [`SuspendingClock`](https://developer.apple.com/documentation/swift/suspendingclock).
     var absolute: Double
 
     /// The number of seconds since the UNIX epoch (1970-01-01 00:00:00 UT).
-    var since1970: Double
-
-    init(encoding instant: borrowing Test.Clock.Instant) {
-      absolute = instant.suspending.rawValue / .seconds(1)
-#if !SWT_NO_UTC_CLOCK
-      since1970 = instant.wall.rawValue / .seconds(1)
-#else
-      since1970 = 0
-#endif
-    }
+    package var since1970: Double
   }
 }
+
+// MARK: - Conversion to/from library types
+
+extension ABI.EncodedInstant {
+  /// Initialize an instance of this type from the given value.
+  ///
+  /// - Parameters:
+  ///   - instant: The instant to initialize this instance from.
+  public init(encoding instant: borrowing Test.Clock.Instant) {
+    absolute = instant.suspending.rawValue / .seconds(1)
+#if !SWT_NO_UTC_CLOCK
+    since1970 = instant.wall.rawValue / .seconds(1)
+#else
+    since1970 = 0
+#endif
+  }
+}
+
+@_spi(ForToolsIntegrationOnly)
+extension SuspendingClock.Instant {
+  /// Initialize this instant to equal an instant from the testing library's
+  /// event stream.
+  ///
+  /// - Parameters:
+  ///   - instant: The encoded instant to initialize this instance from.
+  ///
+  /// The resulting instance is equivalent to the suspending-clock time
+  /// represented by `instant`.
+  public init?<V>(decoding instant: ABI.EncodedInstant<V>) {
+    self = SuspendingClock().systemEpoch + .seconds(instant.absolute)
+  }
+}
+
+// Date.init(decoding:) is in the Foundation overlay.
 
 // MARK: - Codable
 
