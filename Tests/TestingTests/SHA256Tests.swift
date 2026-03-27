c721c554fc96d4d17309828b8c4a3c1a604d406d
Harlan Haskins <harlan@apple.com>
2026-03-26 20:09:19 -0400

    Fix license header

--- Tests/TestingTests/SHA256Tests.swift
+++ Tests/TestingTests/SHA256Tests.swift
@@ -1,8 +1,11 @@
 //
-//  SHA256Tests.swift
-//  swift-testing
+// This source file is part of the Swift.org open source project
 //
-//  Created by Harlan Haskins on 3/26/26.
+// Copyright (c) 2014–2026 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 //
 
 #if canImport(CryptoKit)
