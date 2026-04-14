27c1ed19a1c502040ee4eda20bf2e81fde62f026
Jonathan Grynspan <jgrynspan@apple.com>
2026-04-14 17:33:36 -0400

    Apparently the toolchain compiler doesn't recognize regex expressions

--- Sources/TestingMacros/Support/TestContentGeneration.swift
+++ Sources/TestingMacros/Support/TestContentGeneration.swift
@@ -18,7 +18,11 @@ extension TestContentKind {
   /// a [FourCC](https://en.wikipedia.org/wiki/FourCC) value, or empty trivia if
   /// not.
   fileprivate var commentRepresentation: Trivia {
-    guard let fourCharacterCodeValue, !fourCharacterCodeValue.contains(/\*\//) else {
+    guard let fourCharacterCodeValue else {
+      return []
+    }
+    if fourCharacterCodeValue.contains("*") && fourCharacterCodeValue.contains("/") {
+      // Avoid FourCC strings that might terminate the winged comment early.
       return []
     }
     return .blockComment("/* '\(fourCharacterCodeValue)' */")
