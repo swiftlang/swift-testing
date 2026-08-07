e97a82bb453b7644c272366e73eb8dc6989079f1
Gustavo Medori <mail@gustavo.sh>
2026-08-06 21:21:35 -0600

    Add ability to filter/skip by tags using a special `tag:` prefix (#1531)

--- Sources/Testing/Running/Configuration.TestFilter.swift
+++ Sources/Testing/Running/Configuration.TestFilter.swift
@@ -55,9 +55,16 @@ extension Configuration {
       /// The test filter contains a pattern to predicate test IDs against.
       ///
       /// - Parameters:
-      ///   - patterns: The patterns to predicate test IDs against.
+      ///   - idPatterns: The idPatterns to predicate test IDs against.
       ///   - membership: How to interpret the result when predicating tests.
-      case patterns(_ patterns: [String], membership: Membership)
+      case idPatterns(_ idPatterns: [String], membership: Membership)
+
+      /// The test filter contains a pattern to predicate test tags against.
+      ///
+      /// - Parameters:
+      ///   - tagPatterns: The patterns to predicate test tags against
+      ///   - membership: How to interpret the result when predicating tests.
+      case tagPatterns(_ tagPatterns: [String], membership: Membership)
 #endif
 
       /// The test filter is a combination of other test filter kinds.
@@ -129,9 +136,9 @@ extension Configuration.TestFilter {
   ///
   /// - Parameters:
   ///   - membership: How to interpret the result when predicating tests.
-  ///   - patterns: The patterns, expressed as a `Regex`-compatible regular
+  ///   - idPatterns: The patterns, expressed as a `Regex`-compatible regular
   ///     expressions, to match test IDs against.
-  init(membership: Membership, matchingAnyOf patterns: some Sequence<String>) throws {
+  init(membership: Membership, matchingAnyOf idPatterns: some Sequence<String>) throws {
     // Validate each regular expression by attempting to initialize a `Regex`
     // representing it, but do not preserve it. This type only represents
     // the pattern in the abstract, and is not responsible for actually
@@ -140,11 +147,37 @@ extension Configuration.TestFilter {
     // Performing this validation here currently makes such errors easier to
     // surface when using the SwiftPM entry point. But longer-term, we should
     // make the planning phase throwing and propagate errors from there instead.
-    for pattern in patterns {
+    for pattern in idPatterns {
       _ = try Regex(pattern)
     }
 
-    self.init(_kind: .patterns(Array(patterns), membership: membership))
+    self.init(_kind: .idPatterns(Array(idPatterns), membership: membership))
+  }
+
+  /// Initialize this instance to include tests with tags matching a pattern.
+  ///
+  /// - Parameters:
+  ///   - tagPatterns: The patterns, expressed as a `Regex`-compatible regular
+  ///     expressions, to match test tags against.
+  public init(includingTagsMatching tagPatterns: [String]) throws {
+    // See the comment above in init(membership:matchingAnyOf:) to understand why we construct regexes here.
+    for pattern in tagPatterns {
+      _ = try Regex(pattern)
+    }
+    self.init(_kind: .tagPatterns(tagPatterns, membership: .including))
+  }
+
+  /// Initialize this instance to exclude tests with tags matching a pattern.
+  ///
+  /// - Parameters:
+  ///   - tagPatterns: The patterns, expressed as a `Regex`-compatible regular
+  ///     expressions, to match test tags against.
+  public init(excludingTagsMatching tagPatterns: [String]) throws {
+    // See the comment above in init(membership:matchingAnyOf:) to understand why we construct regexes here.
+    for pattern in tagPatterns {
+      _ = try Regex(pattern)
+    }
+    self.init(_kind: .tagPatterns(tagPatterns, membership: .excluding))
   }
 #endif
 
@@ -252,11 +285,25 @@ extension Configuration.TestFilter.Kind {
       }
       return .function(predicate, membership: membership)
 #if canImport(_StringProcessing)
-    case let .patterns(patterns, membership):
-      nonisolated(unsafe) let regexes = try patterns.map(Regex.init)
+    case let .idPatterns(idPatterns, membership):
+      nonisolated(unsafe) let regexes = try idPatterns.map(Regex.init)
       return .function({ item in
         let id = String(describing: item.test.id)
         return regexes.contains { id.contains($0) }
+      }, membership: membership)
+    case let .tagPatterns(tagPatterns, membership):
+      nonisolated(unsafe) let regexes = try tagPatterns.map(Regex.init)
+      return .function({ item in
+        let tagNames = item.tags.map { tag in
+          switch tag.kind {
+          case let .staticMember(tagName): tagName
+          }
+        }
+        return tagNames.contains(where: { tagName in
+          regexes.contains(where: { regex in
+            tagName.contains(regex)
+          })
+        })
       }, membership: membership)
 #endif
     case let .combination(lhs, rhs, op):
@@ -504,8 +551,10 @@ extension Configuration.TestFilter.Kind {
     case .unfiltered, .testIDs:
       false
 #if canImport(_StringProcessing)
-    case .patterns:
+    case .idPatterns:
       false
+    case .tagPatterns:
+      true
 #endif
     case .tags:
       true
