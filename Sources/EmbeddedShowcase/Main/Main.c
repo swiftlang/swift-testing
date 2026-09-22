a06f8f178bc9cf8aa18e81e2a4469adb8d5a5550
Jonathan Grynspan <jgrynspan@apple.com>
2026-09-21 22:40:32 -0400

    EmbeddedShowcase still needs to compile for non-Embedded

--- Sources/EmbeddedShowcase/Main/Main.c
+++ Sources/EmbeddedShowcase/Main/Main.c
@@ -8,8 +8,18 @@
 // See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 //
 
+#if !SWT_EMBEDDED
+#include <stdio.h>
+#include <stdlib.h>
+#else
 _Noreturn extern void swift_testing_embeddedMain(int argc, char *argv[]);
+#endif
 
 int main(int argc, char *argv[]) {
+#if !SWT_EMBEDDED
+  fputs("This target is intended for Embedded Swift only.\n", stderr);
+  return EXIT_FAILURE;
+#else
   swift_testing_embeddedMain(argc, argv);
+#endif
 }
